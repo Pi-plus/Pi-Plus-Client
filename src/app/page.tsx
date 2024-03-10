2143a02e44a37c611e9bd2f0e092803b9899f157
suminhan123 <suminh324@gmail.com>
2024-03-10 21:41:49 +0900

    feat: 데이터 제목 텍스트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
   const { data } = useGetUserData();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>유저 데이터</h1>
       {data?.map((user) => <h1 key={user.id}>{user?.user_grade}</h1>)}
     </main>
   );
