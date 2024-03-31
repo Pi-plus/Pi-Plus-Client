abc4ef6310b2fe3851b25f01064d80ad8e17dc0d
suminhan123 <suminh324@gmail.com>
2024-03-31 23:15:15 +0900

    design: noto sans kr 폰트 적용 css 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <main className="flex-col items-center justify-between bg-blue-10 w-full">dfg</main>;
+  return (
+    <main className="flex-col items-center justify-between bg-blue-10 w-full">
+      <h1 className="font-bold">가나다라마바사</h1>
+    </main>
+  );
 }
