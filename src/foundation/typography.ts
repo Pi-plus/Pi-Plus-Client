e3a5cf8a0df60de93251195268cf6ca9bb2c022b
sumin han <98216274+suminhan123@users.noreply.github.com>
2024-04-04 10:22:36 +0900

    [UI] 공통 컴포넌트 Typography 생성 (#10)
    * rename: providers 폴더 이동
    
    * design: color foundation 파일 생성
    
    * design: color typography 파일 생성
    
    * feat: typography 키 타입 생성
    
    * feat: typography 컴포넌트 생성

---
+++ src/foundation/typography.ts
@@ -0,0 +1,15 @@
+export const typography = {
+  heading1: 'text-heading1 font-regular leading-heading1',
+  heading2: 'text-heading2 font-bold leading-heading2',
+  title1: 'text-title1 font-bold leading-title1',
+  title2: 'text-title2 font-bold leading-title2',
+  title3: 'text-title3 font-medium leading-title3',
+  body1: 'text-body1 font-medium leading-body1',
+  body2: 'text-body2 font-regular leading-body2',
+  body3: 'text-body3 font-regular leading-body3',
+  caption1: 'text-caption1 font-regular leading-caption1',
+  caption2: 'text-caption2 font-regular leading-caption2',
+  caption3: 'text-caption3 font-regular leading-caption3',
+} as const;
+
+export type TKeyOfTypography = keyof typeof typography;
