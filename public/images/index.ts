6626a26725211d816a37f8e498132caf31b9abb1
suminhan123 <suminh324@gmail.com>
2024-04-07 22:40:36 +0900

    design: 파이플러스 배너 이미지 추가

--- public/images/index.ts
+++ public/images/index.ts
@@ -1,4 +1,4 @@
-type TKeyOfDefaultImages = 'logo';
+type TKeyOfDefaultImages = 'logo' | 'banner1' | 'banner2' | 'banner3';
 
 export type Assets = {
   [key in TKeyOfDefaultImages]: string;
@@ -6,4 +6,7 @@ export type Assets = {
 
 export const defaultImages: Assets = {
   logo: '/images/header-logo.png',
+  banner1: '/images/banner1.png',
+  banner2: '/images/banner2.png',
+  banner3: '/images/banner3.png',
 };
