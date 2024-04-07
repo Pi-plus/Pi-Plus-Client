dbfd72ccd825388ed6936d555fb224c4696224e1
suminhan123 <suminh324@gmail.com>
2024-04-07 22:14:42 +0900

    design: 헤더 디자인 css 구현

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import Image from 'next/image';
+import { defaultImages } from '@public/images';
+
+import { HEADER_TABS } from '@/constants';
+
+import Typography from '../Typography';
+
+const Header = () => {
+  return (
+    <header className="w-full flex justify-between items-center px-12 pt-2">
+      <Image src={defaultImages.logo} width={241} height={90} alt="" />
+      <div className="flex flex-col items-end">
+        <div className="flex items-center mb-5">
+          <Typography label="caption1" color="gray60" className="cursor-pointer" onClick={() => {}}>
+            회원가입
+          </Typography>
+          <Typography label="caption1" color="gray60" className="mx-4">
+            |
+          </Typography>
+          <Typography label="caption1" color="gray60" className="cursor-pointer" onClick={() => {}}>
+            로그인
+          </Typography>
+        </div>
+        <div className="flex items-center gap-20">
+          {HEADER_TABS.map((header) => (
+            <Typography label="title3" key={header.pathname} className="cursor-pointer">
+              {header.title}
+            </Typography>
+          ))}
+        </div>
+      </div>
+    </header>
+  );
+};
+export default Header;
