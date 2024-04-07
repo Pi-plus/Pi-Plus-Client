b7fda66e3ce017057c0b1b8bc17e600f7b18c5d7
suminhan123 <suminh324@gmail.com>
2024-04-07 22:15:03 +0900

    feat: 루트 레이아웃에 헤더 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,8 @@ import Typography from '@/components/Typography';
 
 export default function Home() {
   return (
-    <main className="flex-col items-center justify-between bg-blue-10 w-full">
+    <main className="flex-col items-center justify-between">
       <Typography label="body1" color="blue20">
-        body1
-      </Typography>
-
-      <Typography label="title1" color="blue20">
         body1
       </Typography>
     </main>
