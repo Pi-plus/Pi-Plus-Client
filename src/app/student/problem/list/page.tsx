55560f18e6b54d9127a41b834ce4e4058c431375
suminhan123 <suminh324@gmail.com>
2024-04-28 00:10:45 +0900

    feat: 문제 리스트 페이지 생성

---
+++ src/app/student/problem/list/page.tsx
@@ -0,0 +1,6 @@
+import Typography from '@/components/Typography';
+
+const StudentProblemListPage = () => {
+  return <Typography label="body1">학생 문제 리스트 페이지</Typography>;
+};
+export default StudentProblemListPage;
