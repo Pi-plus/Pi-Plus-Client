49f4ad13657f9a3242c60adf859cc1d011dd3ef6
suminhan123 <suminh324@gmail.com>
2024-04-28 00:10:31 +0900

    feat: 학생 문제 디테일 페이지 생성

---
+++ src/app/student/problem/[id]/page.tsx
@@ -0,0 +1,6 @@
+import Typography from '@/components/Typography';
+
+const StudentProblemDetailPage = () => {
+  return <Typography label="body1">문제 디테일 페이지</Typography>;
+};
+export default StudentProblemDetailPage;
