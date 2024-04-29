e1b17497c17240701f53dbdcec096e6397cc3f06
suminhan123 <suminh324@gmail.com>
2024-04-29 23:55:03 +0900

    feat: preview 페이지에 input 추가

--- src/app/preview/page.tsx
+++ src/app/preview/page.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import Button from '@/components/Button';
+import Input from '@/components/Input';
 import Typography from '@/components/Typography';
+import { useInput } from '@/hooks';
 
 const PreviewPage = () => {
+  const { onChange, value } = useInput('');
   return (
-    <div className="w-full flex-1 flex flex-col">
-      <Typography className="pt-4">버튼 컴포넌트</Typography>
+    <div className="w-full flex-1 flex flex-col px-2">
+      <Typography className="pt-4">button 컴포넌트</Typography>
       <Button className="my-5">버튼</Button>
       <Button className="my-5" disabled>
         버튼
@@ -17,7 +20,11 @@ const PreviewPage = () => {
       <Button className="my-5" type="default">
         버튼
       </Button>
-      <Typography className="pt-4">버튼 컴포넌트</Typography>
+      <Typography className="pt-4">input 컴포넌트</Typography>
+      <Input placeholder="adsg" value={value} onChange={onChange} className="my-5" />
+      <Input placeholder="adsg" value={value} onChange={onChange} className="my-5" type="secondary" />
+
+      <Input placeholder="adsg" value={value} onChange={onChange} errorMessage="에러!" />
     </div>
   );
 };
