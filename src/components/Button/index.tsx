2fcb9ace9faee02ff1332b90b8e67bcd8376a643
suminhan123 <suminh324@gmail.com>
2024-04-29 22:54:26 +0900

    feat: 버튼 컴포넌트 생성

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,36 @@
+import classNames from 'classnames';
+
+import Typography from '../Typography';
+
+import { BUTTON_STYLE } from './constant';
+import type { IButtonProps } from './type';
+
+const Button = ({
+  type = 'secondary',
+  loading = false,
+  disabled = false,
+  children,
+  className,
+  ...rest
+}: IButtonProps) => {
+  return (
+    <button
+      disabled={disabled || loading}
+      className={classNames(
+        className,
+        'w-full bg-blue-30 rounded-xl py-[19px] active:opacity-[0.85] disabled:pointer-events-none disabled:bg-gray-10 disabled:opacity-50',
+        BUTTON_STYLE[type].container,
+      )}
+      {...rest}
+    >
+      {loading ? (
+        <Typography color="gray30">로딩중..</Typography>
+      ) : (
+        <Typography label="body1" color={disabled ? 'gray30' : BUTTON_STYLE[type].textColor}>
+          {children}
+        </Typography>
+      )}
+    </button>
+  );
+};
+export default Button;
