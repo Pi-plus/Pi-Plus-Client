62a5d81b319fa4f797351ff4aad78fbe1ec75d09
suminhan123 <suminh324@gmail.com>
2024-04-29 23:54:34 +0900

    feat: Input 상수 생성

---
+++ src/components/Input/constant.ts
@@ -0,0 +1,8 @@
+import type { InputType } from './type';
+
+export const COMMON_STYLE = 'py-5 w-full rounded-2xl pl-6';
+
+export const INPUT_STYLE: Record<InputType, string> = {
+  default: 'bg-gray-20 text-gray-50',
+  secondary: 'bg-gray-10 text-gray-40',
+};
