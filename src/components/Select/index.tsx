ea877243a5ac6163029619353bc78d3292abe166
suminhan123 <suminh324@gmail.com>
2024-04-30 00:21:11 +0900

    feat: Select 컴포넌트 생성

---
+++ src/components/Select/index.tsx
@@ -0,0 +1,25 @@
+import classNames from 'classnames';
+
+interface SelectOption {
+  value: string;
+  label: string;
+}
+
+interface ISelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
+  options: SelectOption[];
+  placeholder: string;
+}
+
+const Select = ({ placeholder, options, className, ...rest }: ISelectProps) => {
+  return (
+    <select className={classNames(className, 'bg-gray-10 py-4 rounded-xl px-6 text-gray-60')} {...rest}>
+      <option hidden>{placeholder}</option>
+      {options.map((option) => (
+        <option value={option.value} key={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </select>
+  );
+};
+export default Select;
