1bb602fb09211e3b06f1c0048ba8c6d112d030e8
suminhan123 <suminh324@gmail.com>
2024-04-30 22:51:00 +0900

    design: onboarding 이미지 파일 추가

--- public/images/index.ts
+++ public/images/index.ts
@@ -1,4 +1,4 @@
-type TKeyOfDefaultImages = 'logo' | 'banner1' | 'banner2' | 'banner3';
+type TKeyOfDefaultImages = 'logo' | 'banner1' | 'banner2' | 'banner3' | 'onboarding1' | 'onboarding2' | 'onboarding3';
 
 export type Assets = {
   [key in TKeyOfDefaultImages]: string;
@@ -9,4 +9,8 @@ export const defaultImages: Assets = {
   banner1: '/images/banner1.png',
   banner2: '/images/banner2.png',
   banner3: '/images/banner3.png',
+
+  onboarding1: '/images/onboarding1.png',
+  onboarding2: '/images/onboarding2.png',
+  onboarding3: '/images/onboarding3.png',
 };
