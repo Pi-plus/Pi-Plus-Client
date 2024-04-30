5b5693edc47aa0eac876d23605885b426357a9b9
suminhan123 <suminh324@gmail.com>
2024-05-01 00:40:17 +0900

    feat: 홈 온보딩 페이지 애니메이션 적용

--- src/features/landing/components/LandingLayout.tsx
+++ src/features/landing/components/LandingLayout.tsx
@@ -1,10 +1,16 @@
 import type { HTMLAttributes } from 'react';
 import classNames from 'classnames';
 
+import ScrollAnimation from '@/components/Animation';
+
 interface ILandingLayoutProps extends HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
 }
 const LandingLayout = ({ children, className }: ILandingLayoutProps) => {
-  return <div className={classNames('flex w-full gap-28 justify-center py-36', className)}>{children}</div>;
+  return (
+    <ScrollAnimation>
+      <div className={classNames('flex w-full gap-28 justify-center py-36', className)}>{children}</div>
+    </ScrollAnimation>
+  );
 };
 export default LandingLayout;
