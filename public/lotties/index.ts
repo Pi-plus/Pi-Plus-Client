a2d031e31c6f59b231b24ef105b0b18c4781cb26
suminhan123 <suminh324@gmail.com>
2024-04-30 22:50:38 +0900

    design: lottie 파일 추가

--- public/lotties/index.ts
+++ public/lotties/index.ts
@@ -1,6 +1,10 @@
 import clapLottie from './clap.json';
+import emptyLottie from './emptyBox.json';
+import matchingLottie from './matching.json';
+import studyLottie from './study.json';
+import teacherLottie from './teacher.json';
 
-type TKeyOfDefaultLottie = 'clap';
+type TKeyOfDefaultLottie = 'clap' | 'emptyBox' | 'matching' | 'study' | 'teacher';
 
 export type Assets = {
   [key in TKeyOfDefaultLottie]: object;
@@ -8,4 +12,8 @@ export type Assets = {
 
 export const defaultLottie: Assets = {
   clap: clapLottie,
+  emptyBox: emptyLottie,
+  matching: matchingLottie,
+  study: studyLottie,
+  teacher: teacherLottie,
 };
