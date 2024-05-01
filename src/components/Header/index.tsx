e8f5621ac86e3312421443806f063ddb7d898dd0
suminhan123 <suminh324@gmail.com>
2024-05-01 19:05:57 +0900

    feat: 헤더 탭 hover 효과 추가

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useState } from 'react';
 import Image from 'next/image';
 import { defaultImages } from '@public/images';
 
@@ -7,36 +8,47 @@ import { HEADER_TABS } from '@/constants';
 
 import Typography from '../Typography';
 
+import HeaderContent from './HeaderContent';
+
 const Header = () => {
+  const [hover, setHover] = useState<boolean>(false);
   return (
-    <header className="w-full flex justify-between items-center px-12 pt-2 border-b border-blue-30 pb-3">
-      <Image src={defaultImages.logo} width={241} height={90} alt="" />
-      <div className="flex flex-col items-end">
-        <div className="flex items-center mb-5">
-          <Typography label="caption1" color="gray60" className="cursor-pointer" onClick={() => {}}>
-            회원가입
-          </Typography>
-          <Typography label="caption1" color="gray60" className="mx-4">
-            |
-          </Typography>
-          <Typography label="caption1" color="gray60" className="cursor-pointer" onClick={() => {}}>
-            로그인
-          </Typography>
+    <>
+      <header className="w-full flex justify-between items-center px-12 pt-2 border-b border-blue-30 pb-3">
+        <Image src={defaultImages.logo} width={241} height={90} alt="" />
+        <div className="flex flex-col items-end">
+          <div className="flex items-center mb-5">
+            <Typography label="caption1" color="gray60" className="cursor-pointer" onClick={() => {}}>
+              회원가입
+            </Typography>
+            <Typography label="caption1" color="gray60" className="mx-4">
+              |
+            </Typography>
+            <Typography label="caption1" color="gray60" className="cursor-pointer" onClick={() => {}}>
+              로그인
+            </Typography>
+          </div>
+          <div className="flex gap-20 items-center">
+            {HEADER_TABS.map((header) => (
+              <div
+                key={header.pathname}
+                className="flex items-center gap-2"
+                onMouseOver={() => setHover(true)}
+                onFocus={() => {}}
+              >
+                <Typography label="title3" color="black" key={header.pathname} className="cursor-pointer">
+                  {header.title}
+                </Typography>
+                {header.pathname !== 'mypage' && (
+                  <Image src="/svgs/arrowDown.svg" className="cursor-pointer mt-2" alt="" width={11} height={5} />
+                )}
+              </div>
+            ))}
+          </div>
         </div>
-        <div className="flex gap-20 items-center">
-          {HEADER_TABS.map((header) => (
-            <div key={header.pathname} className="flex items-center gap-2">
-              <Typography label="title3" color="black" key={header.pathname} className="cursor-pointer">
-                {header.title}
-              </Typography>
-              {header.pathname !== 'mypage' && (
-                <Image src="/svgs/arrowDown.svg" className="cursor-pointer mt-2" alt="" width={11} height={5} />
-              )}
-            </div>
-          ))}
-        </div>
-      </div>
-    </header>
+      </header>
+      {hover && <HeaderContent setHover={setHover} />}
+    </>
   );
 };
 export default Header;
