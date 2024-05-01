4535c7342ffabebc901c8e8f4ce9a25ab67f994b
suminhan123 <suminh324@gmail.com>
2024-05-01 10:14:21 +0900

    fix: 헤더 탭 상수 변경

--- src/constants/tabs.ts
+++ src/constants/tabs.ts
@@ -1,6 +1,5 @@
 export const HEADER_TABS = [
   { title: '개념학습', pathname: 'concept' },
   { title: '문제 풀기', pathname: 'problem' },
-  { title: '복습하기', pathname: 'review' },
-  { title: '기출문제', pathname: 'test' },
+  { title: '마이 페이지', pathname: 'review' },
 ] as const;
