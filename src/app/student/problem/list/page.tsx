fc7008a3edd37e631c4211b5b7cf2529a6ec26ff
suminhan123 <suminh324@gmail.com>
2024-05-06 00:05:50 +0900

    feat: 디테일 페이지로 navigate

--- src/app/student/problem/list/page.tsx
+++ src/app/student/problem/list/page.tsx
@@ -1,8 +1,26 @@
+'use client';
+
+import Button from '@/components/Button';
+import Select from '@/components/Select';
+import { ROUTES } from '@/constants';
+import { useNavigate } from '@/hooks';
+
 const StudentProblemListPage = () => {
   const columns = ['문제번호', '학년', '단원명', '난이도'];
+  const { push } = useNavigate();
+  const handleNavigateDetail = (id: number) => {
+    push(`${ROUTES.STUDENT_LIST}/${id}`);
+  };
   return (
     <div className="w-full flex flex-col">
-      <div className="flex justify-end">옵션</div>
+      <div className="w-full flex justify-end mb-10 pr-10 mt-14">
+        <div className="flex gap-4 items-center">
+          <Select value="" placeholder="난이도" options={[]} onChange={() => {}} />
+          <Select value="" placeholder="난이도" options={[]} onChange={() => {}} />
+          <Select value="" placeholder="난이도" options={[]} onChange={() => {}} />
+          <Button className="px-6 py-3">문제 검색</Button>
+        </div>
+      </div>
       <div className="w-full flex justify-center">
         <table className="w-[80%]">
           <thead className="bg-gray-10">
@@ -15,13 +33,28 @@ const StudentProblemListPage = () => {
             </tr>
           </thead>
           <tbody>
-            <tr className="w-full border-b border-gray-10 h-[48px] hover:bg-gray-5 cursor-pointer">
+            <tr
+              className="w-full border-b border-gray-10 h-[48px] hover:bg-gray-5 cursor-pointer"
+              onClick={() => handleNavigateDetail(1)}
+            >
               <td className="text-center">10101번</td>
               <td className="text-center">중학교 1학년</td>
               <td className="text-center">2.2 단원명</td>
               <td className="text-center">상</td>
             </tr>
 
+            <tr className="w-full border-b border-gray-10 h-[48px] hover:bg-gray-5 cursor-pointer">
+              <td className="text-center">10102번</td>
+              <td className="text-center">중학교 2학년</td>
+              <td className="text-center">2.3 단원명</td>
+              <td className="text-center">중</td>
+            </tr>
+            <tr className="w-full border-b border-gray-10 h-[48px] hover:bg-gray-5 cursor-pointer">
+              <td className="text-center">10102번</td>
+              <td className="text-center">중학교 2학년</td>
+              <td className="text-center">2.3 단원명</td>
+              <td className="text-center">중</td>
+            </tr>
             <tr className="w-full border-b border-gray-10 h-[48px] hover:bg-gray-5 cursor-pointer">
               <td className="text-center">10102번</td>
               <td className="text-center">중학교 2학년</td>
