0b06feb81e79d25d705153a4f81c427ecb12f033
sumin han <98216274+suminhan123@users.noreply.github.com>
2024-05-05 22:34:27 +0900

    [UI] 수학 개념 준비 페이지 퍼블리싱 (#27)
    * feat: EmptyPage 컴포넌트 생성
    
    * feat: 수학 개념 페이지에 컴포넌트 적용

--- src/app/student/concept/page.tsx
+++ src/app/student/concept/page.tsx
@@ -1,4 +1,8 @@
+'use client';
+
+import { EmptyPage } from '@/components/Pages';
+
 const StudentConceptPage = () => {
-  return <div>개념 </div>;
+  return <EmptyPage title="아직 해당 수학 개념이 준비가 안되었어요" />;
 };
 export default StudentConceptPage;
