7f235a07c75e930ba5d8b0f47cfdb6bc1b498e2a
suminhan123 <suminh324@gmail.com>
2024-05-05 23:34:36 +0900

    design: 수학 개념 리스트 표 디자인

--- src/app/student/problem/list/page.tsx
+++ src/app/student/problem/list/page.tsx
@@ -1,6 +1,37 @@
-import Typography from '@/components/Typography';
-
 const StudentProblemListPage = () => {
-  return <Typography label="body1">학생 문제 리스트 페이지</Typography>;
+  const columns = ['문제번호', '학년', '단원명', '난이도'];
+  return (
+    <div className="w-full flex flex-col">
+      <div className="flex justify-end">옵션</div>
+      <div className="w-full flex justify-center">
+        <table className="w-[80%]">
+          <thead className="bg-gray-10">
+            <tr>
+              {columns.map((column) => (
+                <th scope="col" key={column} className="text-gray-40">
+                  {column}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="w-full border-b border-gray-10 h-[48px] hover:bg-gray-5 cursor-pointer">
+              <td className="text-center">10101번</td>
+              <td className="text-center">중학교 1학년</td>
+              <td className="text-center">2.2 단원명</td>
+              <td className="text-center">상</td>
+            </tr>
+
+            <tr className="w-full border-b border-gray-10 h-[48px] hover:bg-gray-5 cursor-pointer">
+              <td className="text-center">10102번</td>
+              <td className="text-center">중학교 2학년</td>
+              <td className="text-center">2.3 단원명</td>
+              <td className="text-center">중</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 export default StudentProblemListPage;
