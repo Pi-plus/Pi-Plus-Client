1fec7c6cf7a675f8b44cad26225a82d962111a48
suminhan123 <suminh324@gmail.com>
2024-05-09 22:43:21 +0900

    feat: 학생 문제 디테일 페이지 query id param 값 가져오기

--- src/app/student/problem/list/[id]/page.tsx
+++ src/app/student/problem/list/[id]/page.tsx
@@ -1,6 +1,13 @@
 import Typography from '@/components/Typography';
 
-const StudentProblemDetailPage = () => {
-  return <Typography label="body1">문제 디테일 페이지</Typography>;
+interface IStudentProblemDetailProps {
+  params: {
+    id: number;
+  };
+}
+
+const StudentProblemDetailPage = ({ params }: IStudentProblemDetailProps) => {
+  const { id } = params;
+  return <Typography label="body1">{`문제 디테일 페이지${id}`}</Typography>;
 };
 export default StudentProblemDetailPage;
