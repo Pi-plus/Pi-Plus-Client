df92dfcdac75bada1bbc7631d67cc82a27f7b653
suminhan123 <suminh324@gmail.com>
2024-05-19 22:14:31 +0900

    design: 팝업 컨텐츠 스타일 정렬 추가

--- src/components/Popup/PopupContent.tsx
+++ src/components/Popup/PopupContent.tsx
@@ -1,6 +1,6 @@
 import type { PropsWithChildren } from 'react';
 
 const PopupContent = ({ children }: PropsWithChildren) => {
-  return <div className="w-full h-[calc(100%-48px)]">{children}</div>;
+  return <div className="w-full h-[calc(100%-48px)] flex flex-col justify-center items-center">{children}</div>;
 };
 export default PopupContent;
