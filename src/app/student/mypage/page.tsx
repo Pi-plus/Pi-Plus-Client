6d221f02fd2558d3a6ca84fe7921e492a334b2fd
suminhan123 <suminh324@gmail.com>
2024-05-23 14:48:27 +0900

    design: 마이페이지 탭 디자인 추가

--- src/app/student/mypage/page.tsx
+++ src/app/student/mypage/page.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import MyPageBanner from '@/features/mypage/components/Banner';
+import MypageTab from '@/features/mypage/components/Tab';
 
 const StudentMyPage = () => {
   return (
-    <div>
-      <div></div>
+    <div className="flex flex-1">
+      <div className="w-[45%]">
+        <MypageTab name="김광운" type="student" count={5} tablist={['문제 관리', '성취도 확인']} />
+      </div>
       <MyPageBanner />
     </div>
   );
