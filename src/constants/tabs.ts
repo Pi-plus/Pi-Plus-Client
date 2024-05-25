f2a8d037286951ad6e074394dc4d474a3efe12bb
suminhan123 <suminh324@gmail.com>
2024-05-26 00:16:06 +0900

    feat: 탭 타입 생성 및 마이페이지 상수 타입 적용

--- src/constants/tabs.ts
+++ src/constants/tabs.ts
@@ -4,7 +4,7 @@ export type THoverPathname = 'concept' | 'problem';
 export type TValueOfPathname = THoverPathname | 'mypage';
 export type TValueOfTitlename = '개념학습' | '문제 풀기' | '마이 페이지';
 
-export type TTabValue = { title: TValueOfTitlename; pathname: TValueOfPathname; href?: string };
+export type TTabValue = { title: string; pathname: string; href?: string };
 
 export const HEADER_TABS: TTabValue[] = [
   { title: '개념학습', pathname: 'concept', href: ROUTES.STUDENT_CONCEPT },
@@ -12,7 +12,7 @@ export const HEADER_TABS: TTabValue[] = [
   { title: '마이 페이지', pathname: 'mypage', href: ROUTES.STUDENT_MY },
 ];
 
-export const MYPAGE_STUDENT_TABS = [
+export const MYPAGE_STUDENT_TABS: TTabValue[] = [
   { title: '문제 관리', pathname: MYPAGE_STUDENT_ROUTES.PROBLEM_SETTING, href: ROUTES.STUDENT_MY },
   { title: '성취도 확인', pathname: MYPAGE_STUDENT_ROUTES.ACHIEVEMENT, href: ROUTES.STUDENT_MY },
 ];
