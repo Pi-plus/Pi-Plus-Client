0875c6212a96dddabdad3937788b0d04db4304a6
suminhan123 <suminh324@gmail.com>
2024-05-26 00:45:59 +0900

    feat: 헤더 마이페이지 이동 query 추가

--- src/components/Header/HeaderTab.tsx
+++ src/components/Header/HeaderTab.tsx
@@ -2,11 +2,11 @@
 
 import type { HTMLAttributes } from 'react';
 import Image from 'next/image';
-import { usePathname, useRouter } from 'next/navigation';
 import { defaultSvg } from '@public/svgs';
 import classNames from 'classnames';
 
-import { ROUTES, type TValueOfTitlename } from '@/constants';
+import { MYPAGE_STUDENT_ROUTES, ROUTES, type TValueOfTitlename } from '@/constants';
+import { useNavigate } from '@/hooks';
 
 import Typography from '../Typography';
 
@@ -18,8 +18,7 @@ interface IHeaderTabProps extends HTMLAttributes<HTMLDivElement> {
 }
 
 const HeaderTab = ({ isHover, title, hasArrow, href, ...rest }: IHeaderTabProps) => {
-  const router = useRouter();
-  const pathname = usePathname();
+  const { pathname, push } = useNavigate();
   const focus = pathname.startsWith(href);
 
   return (
@@ -32,7 +31,7 @@ const HeaderTab = ({ isHover, title, hasArrow, href, ...rest }: IHeaderTabProps)
         })}
         onClick={() => {
           if (title === '마이 페이지') {
-            router.push(ROUTES.STUDENT_MY);
+            push({ pathname: ROUTES.STUDENT_MY, query: { m: MYPAGE_STUDENT_ROUTES.PROBLEM_SETTING } });
           }
         }}
       >
