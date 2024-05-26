a0d13bc5ad7b468a83f1cd5d1dcf078170a20bd3
suminhan123 <suminh324@gmail.com>
2024-05-26 17:32:39 +0900

    feat: 마이페이지 error, loading 페이지 생성

---
+++ src/app/student/mypage/error.tsx
@@ -0,0 +1,6 @@
+'use client';
+
+const StduentMyErrorPage = () => {
+  return <h1>에러페이지</h1>;
+};
+export default StduentMyErrorPage;
---
+++ src/app/student/mypage/loading.tsx
@@ -0,0 +1,4 @@
+const StudentMyLoadingPage = () => {
+  return <h1>로딩중</h1>;
+};
+export default StudentMyLoadingPage;
