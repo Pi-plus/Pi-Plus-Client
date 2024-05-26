2b620a8596ccce157332c4e4a68158511f1f11dc
suminhan123 <suminh324@gmail.com>
2024-05-26 18:39:46 +0900

    feat: 수학 주관식 더미 데이터 추가

---
+++ src/features/math/mock/data.ts
@@ -0,0 +1,9 @@
+export const data = {
+  answer: [1, 2, 3],
+  answer_type: '1',
+  question: 'https://i.imgur.com/6Nphd7q.png',
+  question_answer: 'https://i.imgur.com/BvQOVZL.png',
+  question_chapter: '1',
+  question_difficulty: 'MEDIUM',
+  textbook_type: '6',
+};
