7b3fd66009da7d476ea22fa0720e7050ad914246
suminhan123 <suminh324@gmail.com>
2024-05-28 13:15:28 +0900

    refactor: 배열로 변환 함수 util 함수에서 적용

--- src/features/math/utils/getCorrectResponse.ts
+++ src/features/math/utils/getCorrectResponse.ts
@@ -1,19 +1,28 @@
 import type { TMathResponse } from '@/apis/math/types';
 import { MATH_RESPONSE } from '@/constants/enums';
 
-export const getCorrectResponse = (solution: number[] | string[], response: string[], type: TMathResponse) => {
+import type { TMathAnswerContent } from '../contexts';
+import type { TFraction } from '../mock/data';
+
+import { getStudentArray } from './getStudentArray';
+
+export const getCorrectResponse = (
+  solution: number[] | string[] | TFraction[],
+  response: TMathAnswerContent,
+  type: TMathResponse,
+) => {
   const stringSolution = solution.map(String);
+  const mathResponse = getStudentArray(response);
   switch (type) {
     case MATH_RESPONSE.fractionResponse:
       console.log(response, solution);
-      break;
 
     case MATH_RESPONSE.multipleChoiceResponse:
       if (solution.length !== response.length) {
         return false;
       }
       const sortedSolution = stringSolution.sort();
-      const sortedResponse = response.sort();
+      const sortedResponse = mathResponse.sort();
       for (let i = 0; i < sortedSolution.length; i++) {
         if (sortedSolution[i] !== sortedResponse[i]) {
           return false;
@@ -27,7 +36,7 @@ export const getCorrectResponse = (solution: number[] | string[], response: stri
       }
 
       for (let i = 0; i < stringSolution.length; i++) {
-        if (stringSolution[i] !== response[i]) {
+        if (stringSolution[i] !== mathResponse[i]) {
           return false;
         }
       }
