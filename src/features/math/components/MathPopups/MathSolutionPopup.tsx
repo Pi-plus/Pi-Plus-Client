f63514a5a31ed81c80842cefaea6f7f06340a6b5
sumin han <98216274+suminhan123@users.noreply.github.com>
2024-05-28 22:42:22 +0900

    [FIX] 팝업 해설 이미지 잘리는 문제 해결 (#50)
    fix: 팝업 해설 이미지 div 디자인 수정

--- src/features/math/components/MathPopups/MathSolutionPopup.tsx
+++ src/features/math/components/MathPopups/MathSolutionPopup.tsx
@@ -14,7 +14,7 @@ interface IMathSolutionPopup {
 }
 const MathSolutionPopup = ({ isOpen, onClose, title, mathSrc, solutionSrc }: IMathSolutionPopup) => {
   return (
-    <Modal isShow={isOpen} onClose={onClose} className="w-[90%] h-[90%] p-7 flex items-center justify-between">
+    <Modal isShow={isOpen} onClose={onClose} className="w-[90%] h-[90%] p-7 flex justify-between items-start">
       <Image
         className="absolute top-6 right-6 cursor-pointer"
         src={defaultSvg.cancel}
@@ -27,8 +27,8 @@ const MathSolutionPopup = ({ isOpen, onClose, title, mathSrc, solutionSrc }: IMa
         <SectionTitle className="mb-14 mt-10" title={title} subTitle="단원에 맞는 문제의 해설을 확인해보세요" />
         <Image src={mathSrc} className="w-full mb-3" width={630} height={600} alt="" />
       </div>
-      <div className="w-1/2 h-full flex justify-center items-center">
-        <Image src={solutionSrc} width={400} height={0} alt="" className="h-auto w-full mt-32" layout="responsive" />
+      <div className="w-1/2 flex justify-center items-center mt-10">
+        <Image src={solutionSrc} width={400} height={0} alt="" className="h-auto w-full" layout="responsive" />
       </div>
     </Modal>
   );
