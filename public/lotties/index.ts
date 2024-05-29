63800bd139d0e913c149473ae8587b11c135ec30
suminhan123 <suminh324@gmail.com>
2024-05-29 17:56:09 +0900

    design: 파이플러스 로티 이미지 추가

--- public/lotties/index.ts
+++ public/lotties/index.ts
@@ -1,12 +1,13 @@
 import clapLottie from './clap.json';
 import emptyLottie from './emptyBox.json';
 import matchingLottie from './matching.json';
+import piPlusLottie from './pi-plus.json';
 import sadLottie from './sad.json';
 import studyLottie from './study.json';
 import teacherLottie from './teacher.json';
 import tryLottie from './try.json';
 
-type TKeyOfDefaultLottie = 'clap' | 'emptyBox' | 'matching' | 'study' | 'teacher' | 'sad' | 'try';
+type TKeyOfDefaultLottie = 'clap' | 'emptyBox' | 'matching' | 'study' | 'teacher' | 'sad' | 'try' | 'piPlus';
 
 export type Assets = {
   [key in TKeyOfDefaultLottie]: object;
@@ -20,4 +21,5 @@ export const defaultLottie: Assets = {
   teacher: teacherLottie,
   sad: sadLottie,
   try: tryLottie,
+  piPlus: piPlusLottie,
 };
