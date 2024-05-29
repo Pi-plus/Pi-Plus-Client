2fbb5bf667cee83c7782e3afa338589b5804d1c3
suminhan123 <suminh324@gmail.com>
2024-05-29 21:41:13 +0900

    feat: 유저 role enum 상수 생성

--- src/constants/enums/role.ts
+++ src/constants/enums/role.ts
@@ -1,6 +1,5 @@
-export const enum UserRole {
-  'STUDENT' = 'STUDENT',
-  'TEACHER' = 'TEACHER',
+export const enum USER_ROLE {
+  'STUDENT' = '학생',
+  'TEACHER' = '선생님',
 }
-
-export type TUserRole = keyof typeof UserRole;
+export type TUserRole = '학생' | '선생님';
