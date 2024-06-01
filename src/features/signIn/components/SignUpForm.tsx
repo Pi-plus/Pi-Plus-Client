00d3d29344103c125fa8966ba0aa76e44fcd067b
suminhan123 <suminh324@gmail.com>
2024-06-01 20:37:40 +0900

    feat: form에 react-hook-form 커스텀 훅에 상태및 유효성 룰 적용

--- src/features/signIn/components/SignUpForm.tsx
+++ src/features/signIn/components/SignUpForm.tsx
@@ -1,29 +1,51 @@
-import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
-
 import Button from '@/components/Button';
 import Input from '@/components/Input';
 import MultiTitle from '@/components/MultiTitle';
-import { app } from '@/utils';
+import { useNavigate } from '@/hooks';
+
+import { useSignUpForm } from '../hooks';
 
 const SignUpForm = () => {
+  const { push } = useNavigate();
+  const {
+    register,
+    getValues,
+    setError,
+    errors,
+    isDisabled,
+    isMinLength,
+    isEmail,
+    isRequiredEmail,
+    isRequiredPassword,
+  } = useSignUpForm();
   const handleClickSignIn = async () => {
-    const auth = getAuth(app);
-    try {
-      const createdUser = await createUserWithEmailAndPassword(auth, 'hansoom3315@naver.com', '123456789###');
-      console.log(createdUser);
-    } catch (error) {
-      console.error(error);
-    }
+    const result = getValues();
+    console.log('결과', result);
   };
   return (
-    <>
-      <MultiTitle title="계정 회원가입을 해주세요" subTitle="이메일 및 비밀번호를 입력해주세요" />
-      <Input placeholder="이메일을 입력해주세요" title="이메일" className="mb-4" />
-      <Input placeholder="비밀번호를 입력해주세요" title="비밀번호" />
-      <Button onClick={handleClickSignIn} className="mt-10">
-        회원가입
+    <form onSubmit={handleClickSignIn} className="flex flex-col">
+      <MultiTitle title="계정 회원가입을 해주세요" subTitle="생성할 계정의 이메일 및 비밀번호를 입력해주세요" />
+      <Input
+        {...register('email', {
+          required: isRequiredEmail,
+          pattern: isEmail(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i),
+        })}
+        placeholder="이메일을 입력해주세요"
+        title="이메일"
+        type="email"
+        errorMessage={errors.email?.message}
+      />
+      <Input
+        {...register('password', { required: isRequiredPassword, minLength: isMinLength(6) })}
+        placeholder="비밀번호를 입력해주세요"
+        type="password"
+        title="비밀번호"
+        errorMessage={errors.password?.message}
+      />
+      <Button className="mt-10" disabled={isDisabled}>
+        다음
       </Button>
-    </>
+    </form>
   );
 };
 export default SignUpForm;
