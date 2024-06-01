a36b426ad20567a4d16d5617ddcd6b7eef0a3c32
suminhan123 <suminh324@gmail.com>
2024-06-02 02:42:32 +0900

    feat: 선생님 query key 상수 생성

--- src/constants/queryKey.ts
+++ src/constants/queryKey.ts
@@ -2,3 +2,4 @@ import { createQueryKeys } from '@/utils';
 
 export const USER_KEYS = createQueryKeys('user');
 export const STUDENT_KEYS = createQueryKeys('student');
+export const TEACHER_KEYS = createQueryKeys('teacher');
