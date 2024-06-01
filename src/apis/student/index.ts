b687af9d12e14a48914376e7ba6c38130a7f675b
suminhan123 <suminh324@gmail.com>
2024-06-02 02:16:01 +0900

    feat: 학생 데이터 추가 api 함수 생성

--- src/apis/student/index.ts
+++ src/apis/student/index.ts
@@ -1,15 +1,21 @@
 import type { CollectionReference, DocumentData } from 'firebase/firestore';
-import { collection, getDocs } from 'firebase/firestore';
+import { addDoc, collection, getDocs } from 'firebase/firestore';
 
 import { db } from '@/utils';
 
-import type { TUserResponse } from '../users/types';
+import type { TUserRequest, TUserResponse } from '../users/types';
+
+const studentRef: CollectionReference<DocumentData, DocumentData> = collection(db, 'student');
 
 const studentApi = {
   get: async (): Promise<TUserResponse[]> => {
-    const usersRef: CollectionReference<DocumentData, DocumentData> = collection(db, 'student');
-    const data = await getDocs(usersRef);
+    const data = await getDocs(studentRef);
     const result = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+    return result;
+  },
+
+  post: async (body: TUserRequest) => {
+    const result = await addDoc(studentRef, body);
     return result;
   },
 };
