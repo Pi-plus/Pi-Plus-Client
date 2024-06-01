103728b932f2551d8531d3a06624ff4e508ef06b
suminhan123 <suminh324@gmail.com>
2024-06-01 19:29:59 +0900

    feat: 쿼리 form 에 맞는 SignupForm 컴포넌트 분기처리

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,13 +2,14 @@
 
 import PiPlusFormLayout from '@/components/PiPlusFormLayout';
 import { SIGN_UP_ROUTES } from '@/constants';
-import { SelectForm, StudentForm, TeacherForm } from '@/features/signIn/components';
+import { SelectForm, SignUpForm, StudentForm, TeacherForm } from '@/features/signIn/components';
 import { useNavigate } from '@/hooks';
 
 const SignUpPage = () => {
   const { query } = useNavigate();
   return (
     <PiPlusFormLayout>
+      {query.step === SIGN_UP_ROUTES.FORM && <SignUpForm />}
       {query.step === SIGN_UP_ROUTES.SELECT && <SelectForm />}
       {query.step === SIGN_UP_ROUTES.STUDENT && <StudentForm />}
       {query.step === SIGN_UP_ROUTES.TEACHER && <TeacherForm />}
