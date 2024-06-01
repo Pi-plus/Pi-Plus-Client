f34976d17007611f4b969e0500a590f09f2c0e76
suminhan123 <suminh324@gmail.com>
2024-06-02 03:03:39 +0900

    feat: 로그인 form 로직 연결

--- src/features/login/components/LoginForm.tsx
+++ src/features/login/components/LoginForm.tsx
@@ -1,17 +1,71 @@
+import { useState } from 'react';
 import Link from 'next/link';
 
+import authApi from '@/apis/auth';
 import Button from '@/components/Button';
 import Input from '@/components/Input';
 import MultiTitle from '@/components/MultiTitle';
 import Typography from '@/components/Typography';
 import { ROUTES } from '@/constants';
+import { useSignUpForm } from '@/features/signup/hooks';
+import { useNavigate } from '@/hooks';
 
 const LoginForm = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { push } = useNavigate();
+  const {
+    register,
+    getValues,
+    setValue,
+    errors,
+    isDisabled,
+    isMinLength,
+    isEmail,
+    isRequiredEmail,
+    isRequiredPassword,
+  } = useSignUpForm();
+
+  const handleClickLogin = () => {
+    setIsLoading(true);
+    const formValues = getValues();
+    authApi.login(formValues).then((result) => {
+      if (result) {
+        // TODO: 토스트로 에러 처리하기
+        setIsLoading(false);
+      } else {
+        setValue('email', '');
+        setValue('password', '');
+        push(ROUTES.HOME);
+      }
+    });
+  };
   return (
-    <>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleClickLogin();
+      }}
+      className="flex flex-col"
+    >
       <MultiTitle title="계정 로그인을 해주세요" subTitle="아이디 및 비밀번호를 입력해주세요" />
-      <Input placeholder="아이디를 입력해주세요" title="아이디" className="mb-4" />
-      <Input placeholder="비밀번호를 입력해주세요" title="비밀번호" />
+      <Input
+        {...register('email', {
+          required: isRequiredEmail,
+          pattern: isEmail(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i),
+        })}
+        placeholder="이메일을 입력해주세요"
+        title="이메일"
+        className="mb-4"
+        type="email"
+        errorMessage={errors.email?.message}
+      />
+      <Input
+        {...register('password', { required: isRequiredPassword, minLength: isMinLength(6) })}
+        placeholder="비밀번호를 입력해주세요"
+        title="비밀번호"
+        type="password"
+        errorMessage={errors.password?.message}
+      />
       <div className="flex justify-center items-center my-14">
         <Typography>아직 회원아니신가요?</Typography>
         <Link href={ROUTES.SIGN_UP}>
@@ -20,8 +74,10 @@ const LoginForm = () => {
           </Typography>
         </Link>
       </div>
-      <Button onClick={() => {}}>로그인</Button>
-    </>
+      <Button loading={isLoading} disabled={isDisabled} onClick={() => {}}>
+        로그인
+      </Button>
+    </form>
   );
 };
 export default LoginForm;
