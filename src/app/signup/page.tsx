4c2a809ccc50ed85997f25a5e6207a33e8cf399b
suminhan123 <suminh324@gmail.com>
2024-06-02 15:28:33 +0900

    refactor: userInfo 와 회원가입 signIn url로 합치기

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,13 +1,18 @@
 'use client';
 
-import PiPlusFormLayout from '@/components/PiPlusFormLayout';
-import { SignUpForm } from '@/features/signup/components';
+import { SIGN_UP_ROUTES } from '@/constants';
+import { SelectForm, SignUpForm, StudentForm, TeacherForm } from '@/features/signup/components';
+import { useNavigate } from '@/hooks';
 
 const SignUpPage = () => {
+  const { query } = useNavigate();
   return (
-    <PiPlusFormLayout>
-      <SignUpForm />
-    </PiPlusFormLayout>
+    <>
+      {query.step === SIGN_UP_ROUTES.SELECT && <SelectForm />}
+      {query.step === SIGN_UP_ROUTES.STUDENT && <StudentForm />}
+      {query.step === SIGN_UP_ROUTES.TEACHER && <TeacherForm />}
+      {query.step === SIGN_UP_ROUTES.FORM && <SignUpForm />}
+    </>
   );
 };
 export default SignUpPage;
