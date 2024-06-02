2c1b6668b12adf194da5553b74651f4ee5544770
suminhan123 <suminh324@gmail.com>
2024-06-02 15:36:05 +0900

    feat: 선생님 관리 학생 리스트 타입 추가

--- src/apis/teacher/types/index.ts
+++ src/apis/teacher/types/index.ts
@@ -4,4 +4,6 @@ import type { TStudentResponse } from '@/apis/student/types';
  * @descript 선생님 DB 응답 타입
  */
 type TTeacherProperties = 'id' | 'uid' | 'accessToken' | 'user_name';
-export type TTeacherResponse = Pick<TStudentResponse, TTeacherProperties>;
+export type TTeacherResponse = Pick<TStudentResponse, TTeacherProperties> & {
+  manage_student: object[];
+};
