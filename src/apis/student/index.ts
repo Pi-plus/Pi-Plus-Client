78a510024bfed00b2acc6f60e2379ba2341dc7a8
suminhan123 <suminh324@gmail.com>
2024-06-02 15:29:49 +0900

    feat: 학생 유저 정보 타입 생성

--- src/apis/student/index.ts
+++ src/apis/student/index.ts
@@ -1,20 +1,20 @@
 import type { CollectionReference, DocumentData } from 'firebase/firestore';
-import { addDoc, collection, getDocs } from 'firebase/firestore';
+import { addDoc, collection } from 'firebase/firestore';
 
 import { db } from '@/utils';
 
-import type { TUserResponse } from '../users/types';
+import type { TStudentResponse } from './types';
 
 const studentRef: CollectionReference<DocumentData, DocumentData> = collection(db, 'student');
 
 const studentApi = {
-  get: async (): Promise<TUserResponse[]> => {
-    const data = await getDocs(studentRef);
-    const result = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
-    return result;
-  },
+  //get: async (): Promise<TUserResponse[]> => {
+  //  const data = await getDocs(studentRef);
+  //  const result = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+  //  return result;
+  //},
 
-  post: async (body: TUserResponse) => {
+  post: async (body: TStudentResponse) => {
     const result = await addDoc(studentRef, body);
     return result;
   },
