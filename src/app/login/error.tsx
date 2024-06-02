66fa97b737723e25d2e905a6d2c1db087c9a6d19
suminhan123 <suminh324@gmail.com>
2024-06-02 17:32:03 +0900

    feat: 로그인 에러, 로딩 페이지 추가

---
+++ src/app/login/error.tsx
@@ -0,0 +1,6 @@
+'use client';
+
+const LoginErrorPage = () => {
+  return <div>에러 페이지</div>;
+};
+export default LoginErrorPage;
---
+++ src/app/login/loading.tsx
@@ -0,0 +1,6 @@
+'use client';
+
+const LoginLoadingPage = () => {
+  return <div>로딩중..</div>;
+};
+export default LoginLoadingPage;
