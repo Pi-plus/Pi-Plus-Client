23460bcad0a1a198d5de551ca31d686293047ec8
suminhan123 <suminh324@gmail.com>
2024-06-02 14:01:56 +0900

    refactor: 회원가입 form provider 컴포넌트로 이동 및 생성

---
+++ src/app/signup/layout.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import type { PropsWithChildren } from 'react';
+
+import PiPlusFormLayout from '@/components/PiPlusFormLayout';
+import { SignUpFormProvider } from '@/features/signup/contexts';
+
+const SignupLayoutPage = ({ children }: PropsWithChildren) => {
+  return (
+    <SignUpFormProvider>
+      <PiPlusFormLayout>{children}</PiPlusFormLayout>
+    </SignUpFormProvider>
+  );
+};
+export default SignupLayoutPage;
---
+++ src/features/signup/contexts/index.ts
@@ -0,0 +1,1 @@
+export { default as SignUpFormProvider } from './SignUpFormProvider';
