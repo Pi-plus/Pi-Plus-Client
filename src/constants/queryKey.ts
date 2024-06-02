3bf893861c32b87dd1a2c4ba1d74bba42918de24
suminhan123 <suminh324@gmail.com>
2024-06-02 22:14:46 +0900

    feat: 수학 문제 쿼리 키 생성

--- src/constants/queryKey.ts
+++ src/constants/queryKey.ts
@@ -3,3 +3,4 @@ import { createQueryKeys } from '@/utils';
 export const USER_KEYS = createQueryKeys('user');
 export const STUDENT_KEYS = createQueryKeys('student');
 export const TEACHER_KEYS = createQueryKeys('teacher');
+export const MATH_KEYS = createQueryKeys('math');
