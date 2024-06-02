da5f0f26043453e5973e97f1716c231ad455bf7e
suminhan123 <suminh324@gmail.com>
2024-06-02 13:50:33 +0900

    feat: 로그인 api 에러 분기 처리로 에러 메시지 return 로직 추가

--- src/apis/auth/index.ts
+++ src/apis/auth/index.ts
@@ -3,23 +3,32 @@ import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'fire
 
 import { auth } from '@/utils';
 
-import type { ISignUpRequest } from './types';
+import type { IEmailPasswordForm } from './types';
 
 const authApi = {
-  post: async (body: ISignUpRequest) => {
+  post: async (body: IEmailPasswordForm) => {
     try {
-      await createUserWithEmailAndPassword(auth, body.email, body.password);
+      const result = await createUserWithEmailAndPassword(auth, body.email, body.password);
+      console.log('uid', result.user.uid);
     } catch (error) {
       const authError = error as AuthError;
       return authError;
     }
   },
-  login: async (body: ISignUpRequest) => {
+  login: async (body: IEmailPasswordForm) => {
     try {
       const result = await signInWithEmailAndPassword(auth, body.email, body.password);
       console.log(result);
+      console.log('uid', result.user.uid);
     } catch (error) {
       const authError = error as AuthError;
+      switch (authError.code) {
+        case 'auth/invalid-credential':
+          return '입력한 이메일은 존재하지 않습니다';
+        case 'auth/wrong-password':
+          return '입력한 이메일과 비밀번호가 일치하지 않습니다';
+      }
+
       return authError;
     }
   },
