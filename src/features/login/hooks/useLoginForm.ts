f80199e637f6646860c20c846fbb51756e998dd8
suminhan123 <suminh324@gmail.com>
2024-06-02 13:20:40 +0900

    refactor: 로그인 유효성 검사 커스텀 훅으로 분리

---
+++ src/features/login/hooks/useLoginForm.ts
@@ -0,0 +1,48 @@
+import { useCallback } from 'react';
+import { useForm } from 'react-hook-form';
+
+import type { IEmailPasswordForm } from '@/apis/auth/types';
+
+export const useLoginForm = () => {
+  const {
+    register,
+    getValues,
+    setError,
+    setValue,
+    formState: { isDirty, isValid, errors },
+  } = useForm<IEmailPasswordForm>({
+    mode: 'onChange',
+  });
+  const isDisabled = !isDirty || !isValid;
+
+  const isMinLength = useCallback((minLength: number) => {
+    return {
+      value: minLength,
+      message: `${minLength}이상 글자여야 합니다.`,
+    };
+  }, []);
+
+  const isEmail = useCallback((pattern: RegExp) => {
+    return {
+      value: pattern,
+      message: '이메일 형식이 아닙니다.',
+    };
+  }, []);
+
+  const isRequiredEmail = '이메일을 무조건 입력해주어야 합니다.';
+
+  const isRequiredPassword = '비밀번호를 무조건 입력해주어야 합니다.';
+
+  return {
+    register,
+    getValues,
+    setError,
+    setValue,
+    errors,
+    isDisabled,
+    isMinLength,
+    isEmail,
+    isRequiredEmail,
+    isRequiredPassword,
+  };
+};
