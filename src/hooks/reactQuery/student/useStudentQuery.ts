01c061f139bad5eac64e4318872639bee20e4378
suminhan123 <suminh324@gmail.com>
2024-06-03 23:22:11 +0900

    refactor: 서버,클라이언트 api 코드 분리 후 index.ts 삭제

--- src/hooks/reactQuery/student/useStudentQuery.ts
+++ src/hooks/reactQuery/student/useStudentQuery.ts
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 
-import studentApi from '@/apis/student';
+import { studentApi } from '@/apis/student/student.client';
 import type { TStudentResponse } from '@/apis/student/types';
 import { STUDENT_KEYS } from '@/constants';
 
