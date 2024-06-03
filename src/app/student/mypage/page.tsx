966d0a98ef5a174a14c79241741028959b47a283
suminhan123 <suminh324@gmail.com>
2024-06-03 22:59:40 +0900

    refactor: 학생 전체 마이페이지 feature 폴더로 파일 생성

--- src/app/student/mypage/page.tsx
+++ src/app/student/mypage/page.tsx
@@ -1,42 +1,8 @@
 'use client';
 
-import { MYPAGE_STUDENT_ROUTES, MYPAGE_STUDENT_TABS } from '@/constants';
-import { ProblemSettingSection } from '@/features/mypage/components/Sections';
-import AchievementSection from '@/features/mypage/components/Sections/AchievementSection';
-import MypageTab from '@/features/mypage/components/Tab';
-import { useNavigate } from '@/hooks';
-import { useStudentQuery } from '@/hooks/reactQuery/student';
+import { StudentPrivatePage } from '@/features/mypage/components';
 
 const StudentMyPage = () => {
-  const { query } = useNavigate();
-  const { data } = useStudentQuery();
-  return (
-    <div className="flex flex-1">
-      {data &&
-        data.map((student) => (
-          <div key={student.id} className="w-full flex">
-            <div className="w-[35%]">
-              <MypageTab
-                name={student.user_name!}
-                gender="male"
-                type="student"
-                count={student.user_goal!}
-                tablist={MYPAGE_STUDENT_TABS}
-              />
-            </div>
-
-            <div className="w-[65%] px-7 py-5">
-              {query.m === MYPAGE_STUDENT_ROUTES.PROBLEM_SETTING && (
-                <ProblemSettingSection
-                  solveList={student.solve_problem ?? []}
-                  wrongList={student.wrong_problem ?? []}
-                />
-              )}
-              {query.m === MYPAGE_STUDENT_ROUTES.ACHIEVEMENT && <AchievementSection />}
-            </div>
-          </div>
-        ))}
-    </div>
-  );
+  return <StudentPrivatePage />;
 };
 export default StudentMyPage;
