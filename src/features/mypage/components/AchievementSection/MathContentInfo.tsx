728ec29cf6461128658a10ab33feab45bec818bd
suminhan123 <suminh324@gmail.com>
2024-06-04 01:05:51 +0900

    feat: 유저 내용 영역별 수치 더미 데이터 적용

--- src/features/mypage/components/AchievementSection/MathContentInfo.tsx
+++ src/features/mypage/components/AchievementSection/MathContentInfo.tsx
@@ -2,20 +2,27 @@ import Typography from '@/components/Typography';
 import HorizontalBarChart from '@/features/dataAnalysis/components/HorizontalBarChart';
 import PentagonRadarChart from '@/features/dataAnalysis/components/PentagonRadarChart';
 
+import { data } from '../../mocks/data';
+import { getMathTagCountList, getMathTagPercentCountList } from '../../utils/getMathData';
+
 const MathContentInfo = () => {
+  const tagsPercentCount = getMathTagPercentCountList(data.solve_problem ?? [], data.wrong_problem ?? []);
+  const tagsCount = getMathTagCountList(data.solve_problem ?? [], data.wrong_problem ?? []);
   return (
     <div className="w-full flex flex-col p-5 items-start justify-start mt-5 shadow-content rounded-lg">
       <Typography label="title1" className="relative z-20">
         내용 영역별 분석
       </Typography>
       <div className="bg-blue-20 h-2 -mt-2 relative z-10 w-[137px]" />
-      <Typography className="mt-6">오각형 방사 그래프를 통해 수학 내용 영역별 성취도를 확인해보세요</Typography>
+      <Typography className="mt-6">
+        오각형 방사 그래프를 통해 푼 수학 문제들의 내용 영역에 대한 분포를 확인해보세요
+      </Typography>
       <div className="grid grid-cols-2 items-center w-full">
         <div>
-          <PentagonRadarChart />
+          <PentagonRadarChart data={tagsPercentCount} />
         </div>
         <div>
-          <HorizontalBarChart />
+          <HorizontalBarChart data={tagsCount} />
         </div>
       </div>
     </div>
