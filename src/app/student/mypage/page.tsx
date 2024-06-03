6989c5c34395c66e0835c3d7305dc211ef16e83f
suminhan123 <suminh324@gmail.com>
2024-06-03 23:10:08 +0900

    feat: 학생 마이페이지 ssr 적용

--- src/app/student/mypage/page.tsx
+++ src/app/student/mypage/page.tsx
@@ -1,8 +1,13 @@
-'use client';
-
+import { studentServerApi } from '@/apis/student/student.server';
+import PrefetchHydration from '@/components/ReactQuery/PrefetchHydration';
+import { STUDENT_KEYS } from '@/constants';
 import { StudentPrivatePage } from '@/features/mypage/components';
 
 const StudentMyPage = () => {
-  return <StudentPrivatePage />;
+  return (
+    <PrefetchHydration queryKey={STUDENT_KEYS.details()} queryFn={studentServerApi.get}>
+      <StudentPrivatePage />
+    </PrefetchHydration>
+  );
 };
 export default StudentMyPage;
