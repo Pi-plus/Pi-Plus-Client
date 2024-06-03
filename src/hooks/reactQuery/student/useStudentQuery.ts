2b6aab832e85c250d16a29361ba02c5e9756c4c2
suminhan123 <suminh324@gmail.com>
2024-06-03 22:51:58 +0900

    feat: 유저 데이터 get 하는 쿼리 커스텀 훅 생성

--- src/hooks/reactQuery/student/useStudentQuery.ts
+++ src/hooks/reactQuery/student/useStudentQuery.ts
@@ -1,11 +1,12 @@
-//import { useQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 
-//import studentApi from '@/apis/student';
-//import { STUDENT_KEYS } from '@/constants';
+import studentApi from '@/apis/student';
+import type { TStudentResponse } from '@/apis/student/types';
+import { STUDENT_KEYS } from '@/constants';
 
-//export const useStudentQuery = () => {
-//  return useQuery({
-//    queryKey: STUDENT_KEYS.details(),
-//    queryFn: studentApi
-//  });
-//};
+export const useStudentQuery = (uid: string) => {
+  return useQuery<TStudentResponse[] | null, Error>({
+    queryKey: STUDENT_KEYS.detail([{ uid }]),
+    queryFn: () => studentApi.get(uid),
+  });
+};
