f959a33a8671200a18ea5ac3111ee6fa1a433474
suminhan123 <suminh324@gmail.com>
2024-06-04 01:06:38 +0900

    feat: 수학 목표 문제 더미 데이터 적용

--- src/features/mypage/components/AchievementSection/MathGoalInfo.tsx
+++ src/features/mypage/components/AchievementSection/MathGoalInfo.tsx
@@ -4,7 +4,12 @@ import { defaultLottie } from '@public/lotties';
 import Typography from '@/components/Typography';
 import PieChart from '@/features/dataAnalysis/components/PieChart';
 
+import { data } from '../../mocks/data';
+
 const MathGoalInfo = () => {
+  const totalCount = (data.solve_problem?.length ?? 0) + (data.wrong_problem?.length ?? 0);
+  const goalCount = Number(data.user_goal ?? 0);
+  const remainCount = goalCount > totalCount ? goalCount - totalCount : 0;
   return (
     <div className="w-full grid grid-cols-2 gap-5 items-center justify-center mt-5">
       <div className="flex flex-col p-5 shadow-content rounded-lg h-[450px]">
@@ -12,21 +17,32 @@ const MathGoalInfo = () => {
           학생 목표
         </Typography>
         <div className="bg-blue-20 h-2 -mt-2 relative z-10 w-[77px]" />
-        <Typography className="mt-6">김광운 님의 목표 문제는 90문제입니다</Typography>
+        <Typography className="mt-6">{`${data.user_name} 님의 목표 문제는 ${data.user_goal}문제입니다`}</Typography>
         <div className="w-full justify-center items-center flex">
           <div className="w-[80%] mt-3">
-            <PieChart />
+            <PieChart activeCount={totalCount} disableCount={remainCount} />
           </div>
         </div>
       </div>
-      <div className="flex flex-col p-5 shadow-content rounded-lg h-[450px] justify-center items-center">
-        <Lottie loop animationData={defaultLottie.goal} className="w-[200px] -mt-11" play />
-        <Typography className="my-4 -mt-8" label="title1">
-          목표까지 총 20문제 남았습니다
-        </Typography>
-        <Typography>목표까지 얼마 남지 않았어요!</Typography>
-        <Typography>문제 풀기 탭에서 목표까지 남은 문제를 풀어보세요</Typography>
-      </div>
+      {goalCount > totalCount ? (
+        <div className="flex flex-col p-5 shadow-content rounded-lg h-[450px] justify-center items-center">
+          <Lottie loop animationData={defaultLottie.goal} className="w-[200px] -mt-11" play />
+          <Typography className="my-4 -mt-8" label="title1">
+            {`목표까지 총 ${goalCount - totalCount}문제 남았습니다`}
+          </Typography>
+          <Typography>목표까지 얼마 남지 않았습니다!</Typography>
+          <Typography>문제 풀기 탭에서 목표까지 남은 문제를 풀어보세요</Typography>
+        </div>
+      ) : (
+        <div className="flex flex-col p-5 shadow-content rounded-lg h-[450px] justify-center items-center">
+          <Lottie loop animationData={defaultLottie.goal} className="w-[200px] -mt-11" play />
+          <Typography className="my-4 -mt-8" label="title1">
+            최종 목표를 달성하였습니다
+          </Typography>
+          <Typography>목표 달성하기까지 수고하였습니다!</Typography>
+          <Typography>문제 풀기 탭에서 더 많은 문제를 풀어보세요</Typography>
+        </div>
+      )}
     </div>
   );
 };
--- src/features/mypage/components/AchievementSection/TrophyTitle.tsx
+++ src/features/mypage/components/AchievementSection/TrophyTitle.tsx
@@ -3,16 +3,18 @@ import { defaultLottie } from '@public/lotties';
 
 import Typography from '@/components/Typography';
 
+import { data } from '../../mocks/data';
+
 const TrophyTitle = () => {
   return (
     <div className="flex items-center justify-start w-full">
       <Lottie loop animationData={defaultLottie.trophy} className="w-[190px]" play />
       <div className="flex flex-col ml-4">
-        <Typography label="heading2">김광운 님의 성취도 분석 자료</Typography>
+        <Typography label="heading2">{`${data.user_name} 님의 성취도 분석 자료`}</Typography>
         <Typography label="body1" className="mt-2">
           풀었던 문제를 기반으로 파이플러스가 분석한 자료입니다
         </Typography>
-        <Typography label="body1">김광운 님의 취약한 수학 유형을 파악해보세요</Typography>
+        <Typography label="body1">{`${data.user_name} 님의 취약한 수학 유형을 파악해보세요`}</Typography>
       </div>
     </div>
   );
