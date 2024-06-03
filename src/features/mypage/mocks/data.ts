d4630c3e0e1e3fba8a041ca76c2c24204d7a2cb6
suminhan123 <suminh324@gmail.com>
2024-06-04 01:04:20 +0900

    test: 학생 성취도 더미 데이터 추가

---
+++ src/features/mypage/mocks/data.ts
@@ -0,0 +1,38 @@
+import type { TStudentResponse } from '@/apis/student/types';
+
+export const data: TStudentResponse = {
+  id: 'A2345ASDFGA#',
+  user_name: '한수민',
+  user_school: '광운중학교',
+  user_goal: '90',
+  solve_problem: [
+    { id: '2349234', tag: '0' },
+    { id: '234923412', tag: '2' },
+    { id: '23234', tag: '3' },
+    { id: '2349234', tag: '0' },
+    { id: '234923412', tag: '2' },
+    { id: '23234', tag: '4' },
+    { id: '2349234', tag: '1' },
+    { id: '234923412', tag: '2' },
+    { id: '23234', tag: '3' },
+    { id: '2349234', tag: '0' },
+    { id: '234923412', tag: '2' },
+    { id: '23234', tag: '3' },
+  ],
+  wrong_problem: [
+    { id: '2349234', tag: '0' },
+    { id: '234923412', tag: '2' },
+    { id: '23234', tag: '3' },
+    { id: '2349234', tag: '0' },
+    { id: '234923412', tag: '2' },
+    { id: '23234', tag: '4' },
+    { id: '2349234', tag: '1' },
+    { id: '234923412', tag: '2' },
+    { id: '23234', tag: '3' },
+    { id: '2349234', tag: '0' },
+    { id: '234923412', tag: '2' },
+    { id: '23234', tag: '3' },
+  ],
+
+  uid: '123dfg35sdga',
+};
