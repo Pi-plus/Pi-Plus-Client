d10a558da105ba470511866870bc033ec078892f
suminhan123 <suminh324@gmail.com>
2024-06-03 23:01:56 +0900

    fix: 선생님 DB 응답 타입 수정

--- src/apis/teacher/types/index.ts
+++ src/apis/teacher/types/index.ts
@@ -3,7 +3,7 @@ import type { TStudentResponse } from '@/apis/student/types';
 /**
  * @descript 선생님 DB 응답 타입
  */
-type TTeacherProperties = 'id' | 'uid' | 'accessToken' | 'user_name';
+type TTeacherProperties = 'id' | 'uid' | 'user_name';
 export type TTeacherResponse = Pick<TStudentResponse, TTeacherProperties> & {
   manage_student: object[];
 };
