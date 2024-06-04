03769e7d7e845fd164ca8e095a9172a843bc8761
suminhan123 <suminh324@gmail.com>
2024-06-04 11:26:23 +0900

    feat: 틀린/맞은 문제 수정할 때 유저 데이터 get invalidateQueries 추가

--- src/hooks/reactQuery/student/useWrongProblemPutStudentMutation.ts
+++ src/hooks/reactQuery/student/useWrongProblemPutStudentMutation.ts
@@ -1,10 +1,15 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { studentApi } from '@/apis/student/student.client';
 import type { TProblemContent } from '@/apis/student/types';
+import { STUDENT_KEYS } from '@/constants';
 
 export const useWrongProblemPutStudentMutation = (body: TProblemContent) => {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: () => studentApi.wrongProblemPut(body),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: STUDENT_KEYS.details() });
+    },
   });
 };
