4d733b9f84690f68c636b3965a93190151fb6e83
suminhan123 <suminh324@gmail.com>
2024-06-05 22:58:46 +0900

    feat: 클립 보드 복사 후 토스트 보이는 로직 추가

--- src/features/mypage/components/MyInfoSection/index.tsx
+++ src/features/mypage/components/MyInfoSection/index.tsx
@@ -4,15 +4,26 @@ import { defaultLottie } from '@public/lotties';
 import Button from '@/components/Button';
 import Input from '@/components/Input';
 import Typography from '@/components/Typography';
+import { showSuccessToast } from '@/utils';
 
 const MyInfoSection = ({ id }: { id: string }) => {
+  const handleUidCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(id);
+      showSuccessToast('클립보드에 성공적으로 복사되었어요');
+    } catch (error) {
+      showSuccessToast('클립보드 복사에 실패했어요');
+    }
+  };
   return (
     <div className="w-full flex flex-col justify-center items-center px-36">
       <Lottie loop className="w-[40%]" animationData={defaultLottie.teacher} play />
       <Typography label="heading2">학생의 고유한 코드를 복사해 선생님께 전달해주세요</Typography>
       <Typography label="body2">선생님은 코드를 통해 학생을 추가해 성취도 관리할 수 있어요</Typography>
       <Input defaultValue={id} className="mt-8" disabled={true} />
-      <Button className="mt-8">클립 보드 복사</Button>
+      <Button className="mt-8" onClick={handleUidCopy}>
+        클립 보드 복사
+      </Button>
     </div>
   );
 };
