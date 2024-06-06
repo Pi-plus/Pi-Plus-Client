9e42663386e556c550422809eedfcd5c8201394c
sumin han <98216274+suminhan123@users.noreply.github.com>
2024-06-06 15:10:55 +0900

    [FEAT] 선생님 페이지에서 학생 성취도 모달 컴포넌트 생성 (#88)
    * feat: 학생 관리 baseurl 상수 추가
    
    * style: piechart 컨테이너 스타일 수정
    
    * feat: 학생 성취도 모달 컴포넌트 추가

--- src/features/mypage/components/AchievementSection/MathGoalInfo.tsx
+++ src/features/mypage/components/AchievementSection/MathGoalInfo.tsx
@@ -12,14 +12,16 @@ const MathGoalInfo = ({ data }: { data: TStudentResponse }) => {
   return (
     <div className="w-full grid grid-cols-2 gap-5 items-center justify-center mt-5">
       <div className="flex flex-col p-5 shadow-content rounded-lg h-[450px]">
-        <Typography label="title1" className="relative z-20">
-          학생 목표
-        </Typography>
-        <div className="bg-blue-20 h-2 -mt-2 relative z-10 w-[77px]" />
-        <Typography className="mt-6">{`${data.user_name} 님의 목표 문제는 ${data.user_goal}문제입니다`}</Typography>
-        <div className="w-full justify-center items-center flex">
-          <div className="w-[80%] mt-3">
-            <PieChart activeCount={totalCount} disableCount={remainCount} />
+        <div className="size-full flex flex-col">
+          <Typography label="title1" className="relative z-20">
+            학생 목표
+          </Typography>
+          <div className="bg-blue-20 h-2 -mt-2 relative z-10 w-[77px]" />
+          <Typography className="mt-6">{`${data.user_name} 님의 목표 문제는 ${data.user_goal}문제입니다`}</Typography>
+          <div className="size-full justify-center items-center flex">
+            <div className="size-[80%]">
+              <PieChart activeCount={totalCount} disableCount={remainCount} />
+            </div>
           </div>
         </div>
       </div>
