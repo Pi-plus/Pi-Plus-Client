f77726c3d3c6e13e4b69c345fd4a662964c234fc
suminhan123 <suminh324@gmail.com>
2024-06-06 13:50:15 +0900

    refactor: api 통신을 page 로 이동해 prop으로 전달

--- src/features/mypage/components/AchievementSection/index.tsx
+++ src/features/mypage/components/AchievementSection/index.tsx
@@ -1,17 +1,21 @@
 'use client';
 
+import { useStudentQuery } from '@/hooks/reactQuery/student';
+
 import MathContentInfo from './MathContentInfo';
 import MathCountInfo from './MathCountInfo';
 import MathGoalInfo from './MathGoalInfo';
 import TrophyTitle from './TrophyTitle';
 
 const AchievementSection = () => {
+  const { data } = useStudentQuery();
+  const [studentData] = data ?? [{}];
   return (
     <div className="w-full px-5">
-      <TrophyTitle />
-      <MathCountInfo />
-      <MathContentInfo />
-      <MathGoalInfo />
+      <TrophyTitle data={studentData} />
+      <MathCountInfo data={studentData} />
+      <MathContentInfo data={studentData} />
+      <MathGoalInfo data={studentData} />
     </div>
   );
 };
