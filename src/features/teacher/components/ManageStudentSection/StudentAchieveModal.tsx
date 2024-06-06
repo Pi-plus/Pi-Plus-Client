5f66b16b972829bdcdeaccca761abd9ae2b315a1
suminhan123 <suminh324@gmail.com>
2024-06-06 15:06:19 +0900

    feat: 학생 성취도 모달 컴포넌트 추가

--- src/features/teacher/components/ManageStudentSection/StudentAchieveModal.tsx
+++ src/features/teacher/components/ManageStudentSection/StudentAchieveModal.tsx
@@ -1,13 +1,35 @@
 import Image from 'next/image';
 import { defaultSvg } from '@public/svgs';
 
+import type { TStudentResponse } from '@/apis/student/types';
 import Modal from '@/components/Modal';
+import MathContentInfo from '@/features/mypage/components/AchievementSection/MathContentInfo';
+import MathCountInfo from '@/features/mypage/components/AchievementSection/MathCountInfo';
+import MathGoalInfo from '@/features/mypage/components/AchievementSection/MathGoalInfo';
+import TrophyTitle from '@/features/mypage/components/AchievementSection/TrophyTitle';
 
 interface IStudentAchievePopupProps {
   isOpen: boolean;
   onClose: () => void;
 }
 const StudentAchievePopup = ({ isOpen, onClose }: IStudentAchievePopupProps) => {
+  const data: TStudentResponse = {
+    id: '123',
+    user_name: '김광운',
+    user_goal: '30',
+    solve_problem: [
+      {
+        id: '124345',
+        tag: '2',
+      },
+    ],
+    wrong_problem: [
+      {
+        id: '124345',
+        tag: '2',
+      },
+    ],
+  };
   return (
     <Modal isShow={isOpen} onClose={onClose} className="w-[90%] h-[90%] gap-20 p-7 flex justify-between items-start">
       <Image
@@ -18,6 +40,13 @@ const StudentAchievePopup = ({ isOpen, onClose }: IStudentAchievePopupProps) => 
         height={24}
         alt=""
       />
+
+      <div className="w-full px-5">
+        <TrophyTitle data={data} />
+        <MathCountInfo data={data} />
+        <MathContentInfo data={data} />
+        <MathGoalInfo data={data} />
+      </div>
     </Modal>
   );
 };
