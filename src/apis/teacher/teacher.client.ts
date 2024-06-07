259225520af97bf7b2213e24a79b015f3351a93e
suminhan123 <suminh324@gmail.com>
2024-06-07 12:58:08 +0900

    feat: 클라리언트 단에서 teacher get, post api 생성

--- src/apis/teacher/teacher.client.ts
+++ src/apis/teacher/teacher.client.ts
@@ -1,21 +1,32 @@
 import type { CollectionReference, DocumentData } from 'firebase/firestore';
+import { getDocs, query } from 'firebase/firestore';
+import { where } from 'firebase/firestore';
 import { addDoc, collection } from 'firebase/firestore';
 
 import { db } from '@/utils';
+import { getUid } from '@/utils/cookie/manageCookie.client';
 
 import type { TTeacherResponse } from './types';
 
 const teacherRef: CollectionReference<DocumentData, DocumentData> = collection(db, 'teacher');
 
 const teacherApi = {
-  //get: async (): Promise<TUserResponse[]> => {
-  //  const data = await getDocs(teacherRef);
-  //  const result = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
-  //  return result;
-  //},
   post: async (body: TTeacherResponse) => {
     const result = await addDoc(teacherRef, body);
     return result;
+  },
+
+  get: async () => {
+    const uid = getUid();
+    const q = query(teacherRef, where('uid', '==', uid));
+    const querySnapshot = await getDocs(q);
+    const userData: TTeacherResponse[] = [];
+
+    querySnapshot.forEach((doc) => {
+      userData.push({ id: doc.id, ...doc.data() });
+    });
+
+    return userData;
   },
 };
 
