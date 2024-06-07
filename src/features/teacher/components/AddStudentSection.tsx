a515ff1b418faa625c75ad680e9409fe242d953e
suminhan123 <suminh324@gmail.com>
2024-06-07 14:17:30 +0900

    feat: 관리 학생 추가 api 연결 후 토스트 띄우기

--- src/features/teacher/components/AddStudentSection.tsx
+++ src/features/teacher/components/AddStudentSection.tsx
@@ -5,9 +5,19 @@ import Button from '@/components/Button';
 import Input from '@/components/Input';
 import Typography from '@/components/Typography';
 import { useInput } from '@/hooks';
+import { useManageStudentPutTeacherMutation } from '@/hooks/reactQuery/teacher';
+import { showSuccessToast } from '@/utils';
 
 const AddStudentSection = () => {
-  const { value, onChange } = useInput('');
+  const { value, onChange, setValue } = useInput('');
+  const { mutateAsync, isSuccess } = useManageStudentPutTeacherMutation();
+
+  const handleAddManageStudent = () => {
+    mutateAsync(value ?? '');
+
+    setValue('');
+    showSuccessToast('관리학생을 성공적으로 추가되었어요');
+  };
   return (
     <div className="size-full flex flex-col justify-center items-center px-36">
       <Lottie loop className="w-[40%]" animationData={defaultLottie.teacher} play />
@@ -16,7 +26,9 @@ const AddStudentSection = () => {
       </Typography>
       <Typography label="body2">학생의 마이 페이지에서 코드를 확인할 수 있어요</Typography>
       <Input className="mt-8" placeholder="학생 코드를 입력해주세요" value={value} onChange={onChange} />
-      <Button className="mt-8">관리 학생 추가</Button>
+      <Button className="mt-8" onClick={handleAddManageStudent} disabled={value.length === 0}>
+        관리 학생 추가
+      </Button>
     </div>
   );
 };
