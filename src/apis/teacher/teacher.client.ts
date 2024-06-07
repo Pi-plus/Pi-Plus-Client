d6005602c1338a498bed84bd72f324717c4189e0
suminhan123 <suminh324@gmail.com>
2024-06-07 14:15:33 +0900

    feat: 학생 uid 가 인자로 들어오면 해당 학생 정보를 get 해 선생님 정보 update 하는 함수 생성

--- src/apis/teacher/teacher.client.ts
+++ src/apis/teacher/teacher.client.ts
@@ -1,8 +1,11 @@
 import type { CollectionReference, DocumentData } from 'firebase/firestore';
+import { doc } from 'firebase/firestore';
+import { updateDoc } from 'firebase/firestore';
 import { getDocs, query } from 'firebase/firestore';
 import { where } from 'firebase/firestore';
 import { addDoc, collection } from 'firebase/firestore';
 
+import { studentApi } from '@/apis/student/student.client';
 import { db } from '@/utils';
 import { getUid } from '@/utils/cookie/manageCookie.client';
 
@@ -27,6 +30,35 @@ const teacherApi = {
     });
 
     return userData;
+  },
+
+  manageStudentPut: async (studentUid: string) => {
+    const uid = getUid();
+    const studentData = await studentApi.getById(studentUid);
+    console.log('학생 정보 받기', studentData[0]);
+    const studentObject = {
+      uid: studentUid,
+      user_name: studentData[0].user_name,
+      user_school: studentData[0].user_school,
+    };
+
+    const q = query(teacherRef, where('uid', '==', uid));
+    const querySnapshot = await getDocs(q);
+
+    if (!querySnapshot.empty) {
+      querySnapshot.forEach(async (docSnapshot) => {
+        const user = docSnapshot.data() as TTeacherResponse;
+        const { manage_student, ...rest } = user;
+        const newManageStudent = [...(manage_student || []), studentObject];
+        const result = {
+          ...rest,
+          manage_student: newManageStudent,
+        };
+
+        const updateTeacherRef = doc(db, 'teacher', docSnapshot.id);
+        await updateDoc(updateTeacherRef, result);
+      });
+    }
   },
 };
 
