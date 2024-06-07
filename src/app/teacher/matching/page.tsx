c3ff9c839921602fc9524ec3bbb8a312d43f0cdd
suminhan123 <suminh324@gmail.com>
2024-06-07 13:21:33 +0900

    feat: prefetchHydration 으로 선생님 마이페이지 감싸 ssr 구현

--- src/app/teacher/matching/page.tsx
+++ src/app/teacher/matching/page.tsx
@@ -1,7 +1,14 @@
+import { teacherServerApi } from '@/apis/teacher/teacher.server';
+import PrefetchHydration from '@/components/ReactQuery/PrefetchHydration';
+import { TEACHER_KEYS } from '@/constants';
 import TeacherMyPage from '@/features/teacher/components/Page';
 
 const TeacherMatchingPage = () => {
-  return <TeacherMyPage />;
+  return (
+    <PrefetchHydration queryKey={TEACHER_KEYS.details()} queryFn={teacherServerApi.get}>
+      <TeacherMyPage />
+    </PrefetchHydration>
+  );
 };
 
 export default TeacherMatchingPage;
