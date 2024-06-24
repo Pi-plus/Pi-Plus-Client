c30463fa4a0260daf1fecf8b1d35ef57dde56bc2
suminhan123 <suminh324@gmail.com>
2024-06-24 17:14:05 +0900

    refactor: 수학 팝업 열고 닫는 로직을 선언적 프로그래밍으로 변경

--- src/features/math/components/MathDetailPage.tsx
+++ src/features/math/components/MathDetailPage.tsx
@@ -9,19 +9,19 @@ import MathTitle from '@/components/MathTitle';
 import Typography from '@/components/Typography';
 import { MATH_RESPONSE } from '@/constants/enums';
 import { SectionTitle } from '@/features/math/components';
+import { MathPopupFactory } from '@/features/math/components/MathPopups';
 import MathProblemContent from '@/features/math/components/MathProblemContent';
+import { MATH_POPUPS } from '@/features/math/constants';
 import type { TMathAnswer } from '@/features/math/contexts';
+import { useMathPopups } from '@/features/math/hooks';
 import { getCorrectResponse } from '@/features/math/utils';
-import { useModal } from '@/hooks';
 import { useMathDetailQuery } from '@/hooks/reactQuery/math';
 import { useSolveProblemPutStudentMutation, useWrongProblemPutStudentMutation } from '@/hooks/reactQuery/student';
 
 import MathForm from './MathForm';
-import { MathCorrectPopup, MathSolutionPopup, MathWrongPopup } from './MathPopups';
 
 const MathDetailPage = ({ id }: { id: string }) => {
   const { data } = useMathDetailQuery(id);
-  console.log(data?.answer);
   const {
     getValues,
     setValue,
@@ -39,6 +39,7 @@ const MathDetailPage = ({ id }: { id: string }) => {
 
   const answer = data?.answer ?? [];
   const inputCount = data?.answer_type === MATH_RESPONSE.fractionResponse ? answer.length * 2 : answer.length;
+  const { openPopup, closePopup } = useMathPopups();
 
   const handleEmptyFormState = () => {
     const initiaArray =
@@ -48,20 +49,80 @@ const MathDetailPage = ({ id }: { id: string }) => {
     setValue('answer', initiaArray);
   };
 
-  const { isOpen: isSolutionOpen, onClose: onSolutionClose, onOpen: onSolutionOpen } = useModal();
-  const { isOpen: isCorrectOpen, onClose: onCorrectClose, onOpen: onCorrectOpen } = useModal();
-  const { isOpen: isWrongOpen, onClose: onWrongClose, onOpen: onWrongOpen } = useModal();
+  const handleOpenMathSolutionPopup = () => {
+    openPopup(MATH_POPUPS.solution, {
+      isOpen: true,
+      onClose: () => {
+        closePopup(MATH_POPUPS.solution);
+      },
+      onConfirm: () => {},
+      content: (
+        <>
+          {data && (
+            <div className="grid grid-cols-2 gap-10 h-full">
+              <div className="flex flex-col items-center justify-center w-full h-full">
+                <SectionTitle
+                  className="mb-14 mt-10"
+                  title={`${id}번의 문제`}
+                  subTitle="단원에 맞는 문제의 해설을 확인해보세요"
+                />
+                <Image src={data.question!} className="w-full mb-3" width={630} height={600} alt="" />
+              </div>
+              <div className="flex justify-center items-center mt-10">
+                <Image
+                  src={data.question_answer!}
+                  width={400}
+                  height={0}
+                  alt=""
+                  className="h-auto w-full"
+                  layout="responsive"
+                />
+              </div>
+            </div>
+          )}
+        </>
+      ),
+    });
+  };
 
   const handleMathResponseSubmit = () => {
     const studentAnswer = getValues('answer');
     handleEmptyFormState();
     // 문제 맞았을 때
     if (data && getCorrectResponse(data.answer ?? [], studentAnswer, data.answer_type ?? '0')) {
-      onCorrectOpen();
+      openPopup(MATH_POPUPS.correct, {
+        isOpen: true,
+        title: '정답입니다!',
+        onClose: () => {
+          closePopup(MATH_POPUPS.correct);
+        },
+        onConfirm: () => {
+          console.log('실행은 되니?');
+          handleOpenMathSolutionPopup();
+        },
+      });
       solveMutate();
       // 문제 틀렸을 때
     } else {
-      onWrongOpen();
+      openPopup(MATH_POPUPS.wrong, {
+        isOpen: true,
+        title: '오답이에요ㅠㅠ',
+        content: (
+          <>
+            <Typography label="title2" className="mt-4">
+              다시 풀어보면 맞을 수 있을 거에요!
+            </Typography>
+            <Typography label="title2">한번 더 시도해보아요!</Typography>
+          </>
+        ),
+        onClose: () => {
+          closePopup(MATH_POPUPS.wrong);
+        },
+        onConfirm: () => {
+          console.log('실행은 되니?');
+          handleOpenMathSolutionPopup();
+        },
+      });
       wrongMutate();
     }
   };
@@ -99,59 +160,7 @@ const MathDetailPage = ({ id }: { id: string }) => {
           답안 제출
         </Button>
 
-        {data && (
-          // 문제 해설 팝업
-          <MathSolutionPopup
-            isOpen={isSolutionOpen}
-            onClose={onSolutionClose}
-            content={
-              <div className="grid grid-cols-2 gap-10 h-full">
-                <div>
-                  <SectionTitle
-                    className="mb-14 mt-10"
-                    title={`${id} 번 문제에 대한 답입니다!`}
-                    subTitle="단원에 맞는 문제의 해설을 확인해보세요"
-                  />
-                  <Image src={data.question!} className="w-full mb-3" width={630} height={600} alt="" />
-                </div>
-                <div className="flex justify-center items-center">
-                  <Image
-                    src={data.question_answer!}
-                    width={400}
-                    height={0}
-                    alt=""
-                    className="h-auto w-full"
-                    layout="responsive"
-                  />
-                </div>
-              </div>
-            }
-          />
-        )}
-
-        {/*문제 맞을 때 팝업*/}
-        <MathCorrectPopup
-          isOpen={isCorrectOpen}
-          onClose={onCorrectClose}
-          onConfirm={onSolutionOpen}
-          title={'정답입니다!'}
-        />
-
-        {/*문제 틀렸을 때 팝업*/}
-        <MathWrongPopup
-          isOpen={isWrongOpen}
-          onClose={onWrongClose}
-          onConfirm={onSolutionOpen}
-          title="오답이에요ㅠㅠ"
-          content={
-            <>
-              <Typography label="title2" className="mt-4">
-                다시 풀어보면 맞을 수 있을 거에요!
-              </Typography>
-              <Typography label="title2">한번 더 시도해보아요!</Typography>
-            </>
-          }
-        />
+        <MathPopupFactory />
       </div>
     </main>
   );
