c655abde858bb860118b50f03e528daa8c381b3c
suminhan123 <suminh324@gmail.com>
2024-06-24 17:09:26 +0900

    feat: 팝업 컴포넌트 prop 타입 생성

--- src/features/math/types/popups.ts
+++ src/features/math/types/popups.ts
@@ -1,10 +1,21 @@
-export interface IMathPopupProps {
+import type { ComponentProps, FunctionComponent } from 'react';
+import type React from 'react';
+
+import type { MathCorrectPopup, MathSolutionPopup, MathWrongPopup } from '@/features/math/components/MathPopups';
+
+export type TMathPopupProps = {
   isOpen: boolean;
   onClose: VoidFunction;
-  onConfirm?: VoidFunction;
+  onConfirm: VoidFunction;
   title?: string;
   content?: React.ReactNode;
-}
-export type IMathPopupPropsWithConfirm = Omit<IMathPopupProps, 'onConfirm'> & {
-  onConfirm: VoidFunction;
 };
+
+export type TPopupsComponent =
+  | ComponentProps<typeof MathCorrectPopup>
+  | ComponentProps<typeof MathSolutionPopup>
+  | ComponentProps<typeof MathWrongPopup>;
+export interface IPopupProps {
+  Component: FunctionComponent<TPopupsComponent>;
+  props: TMathPopupProps;
+}
