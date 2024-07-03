42930d3c25a9df9a04fb5e7873a82a3b1e2cff11
suminhan123 <suminh324@gmail.com>
2024-07-03 21:33:50 +0900

    fix: Button 스토리북 테스트 코드 순서 수정

--- src/components/Button/Button.stories.ts
+++ src/components/Button/Button.stories.ts
@@ -35,16 +35,16 @@ const meta: Meta<typeof Button> = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const Secondary: Story = {
   args: {
-    type: 'default',
+    type: 'secondary',
     children: '버튼',
   },
 };
 
-export const Secondary: Story = {
+export const Default: Story = {
   args: {
-    type: 'secondary',
+    type: 'default',
     children: '버튼',
   },
 };
