76b1019924e6ef286e3ff7c4a6e46c89755ba389
suminhan123 <suminh324@gmail.com>
2024-07-03 22:33:05 +0900

    feat: SelectButton 스토리북 테스트 코드 추가

---
+++ src/components/SelectButton/SelectButton.stories.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+
+import SelectButton from '@/components/SelectButton/index';
+
+const meta: Meta<typeof SelectButton> = {
+  title: 'Components/SelectButton',
+  component: SelectButton,
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    isSelected: {
+      control: false,
+    },
+  },
+};
+export default meta;
+
+const Template = () => {
+  const [isSelected, setIsSelected] = useState<boolean>(false);
+  const handleSelectButton = () => {
+    setIsSelected((prevState: boolean) => !prevState);
+  };
+
+  return <SelectButton onClick={handleSelectButton} label="클릭" isSelected={isSelected} />;
+};
+
+export const Select: StoryObj<typeof Template> = {
+  render: () => <Template />,
+  args: {
+    isSelected: false,
+  },
+};
