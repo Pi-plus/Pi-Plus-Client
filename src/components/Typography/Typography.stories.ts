1101b2ee67c4c4f058ab5e976823a9bd52f852e5
suminhan123 <suminh324@gmail.com>
2024-07-04 16:09:22 +0900

    fix: Typography 스토리북 title 수정

--- src/components/Typography/Typography.stories.ts
+++ src/components/Typography/Typography.stories.ts
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import Typography from '@/components/Typography/index';
 
 const meta: Meta<typeof Typography> = {
-  title: 'Components/Typography',
+  title: 'Typography/Typography',
   component: Typography,
 };
 
