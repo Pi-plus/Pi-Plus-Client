64577b3a8b9514ad084f3bdc3c1d32e74e92cb69
suminhan123 <suminh324@gmail.com>
2024-07-04 17:58:27 +0900

    fix: Input title 수정

--- src/components/Input/Input.stories.ts
+++ src/components/Input/Input.stories.ts
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import Input from '@/components/Input/index';
 
 const meta: Meta<typeof Input> = {
-  title: 'Components/Input',
+  title: 'Input/Input',
   component: Input,
   parameters: {
     layout: 'centered',
