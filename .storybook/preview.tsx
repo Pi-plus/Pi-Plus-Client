603dd562616d432867aaa29a1830a8405fc7ee7d
suminhan123 <suminh324@gmail.com>
2024-07-05 21:53:18 +0900

    feat: decorator 를 통해 PortalProvider 로 감싸기

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,5 +1,7 @@
 import type { Preview } from '@storybook/react';
 
+import { PortalProvider } from '@/components/GlobalPortal';
+
 import '../src/app/globals.css';
 
 const preview: Preview = {
@@ -11,6 +13,14 @@ const preview: Preview = {
       },
     },
   },
+
+  decorators: [
+    (Story) => (
+      <PortalProvider>
+        <Story />
+      </PortalProvider>
+    ),
+  ],
 };
 
 export default preview;
