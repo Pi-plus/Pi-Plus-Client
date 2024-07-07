b0a1ccffb92e1890387a0405aacbc4f10e9fcc10
suminhan123 <suminh324@gmail.com>
2024-07-07 10:18:14 +0900

    style: MultiTitle 스타일 추가

--- src/components/MultiTitle/index.tsx
+++ src/components/MultiTitle/index.tsx
@@ -6,12 +6,12 @@ interface IMultiTitleProps {
 }
 const MultiTitle = ({ title, subTitle }: IMultiTitleProps) => {
   return (
-    <>
+    <div className="flex flex-col">
       <Typography label="heading2">{title}</Typography>
       <Typography label="body2" className="mb-7">
         {subTitle}
       </Typography>
-    </>
+    </div>
   );
 };
 export default MultiTitle;
