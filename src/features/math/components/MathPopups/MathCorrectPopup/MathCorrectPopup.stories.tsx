3271ccd22679faa69889d60e4b755f487c67b948
suminhan123 <suminh324@gmail.com>
2024-07-07 20:53:23 +0900

    refactor: MathCorrectPopup 선언적 프로그래밍 스토리북에 적용

--- src/features/math/components/MathPopups/MathCorrectPopup/MathCorrectPopup.stories.tsx
+++ src/features/math/components/MathPopups/MathCorrectPopup/MathCorrectPopup.stories.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 
 import Button from '@/components/Button';
+import { MathPopupFactory } from '@/features/math/components/MathPopups';
 import MathCorrectPopup from '@/features/math/components/MathPopups/MathCorrectPopup/index';
-import { useModal } from '@/hooks';
+import { MATH_POPUPS } from '@/features/math/constants';
+import { useMathPopups } from '@/features/math/hooks';
 
 const meta: Meta<typeof MathCorrectPopup> = {
   title: 'Popup/MathCorrectPopup',
@@ -12,14 +14,24 @@ const meta: Meta<typeof MathCorrectPopup> = {
 export default meta;
 
 const Template = () => {
-  const { isOpen, onClose, onOpen } = useModal();
-
+  const popup = useMathPopups();
+  const handleOpenCorrectPopup = () => {
+    popup.openPopup(MATH_POPUPS.correct, {
+      title: '정답입니다!',
+      onClose: () => {
+        popup.closePopup(MATH_POPUPS.correct);
+      },
+      onConfirm: () => {
+        popup.closePopup(MATH_POPUPS.correct);
+      },
+    });
+  };
   return (
     <>
       <div className="w-[30%]">
-        <Button onClick={onOpen}>정답 팝업 열기</Button>
+        <Button onClick={handleOpenCorrectPopup}>정답 팝업 열기</Button>
       </div>
-      <MathCorrectPopup isOpen={isOpen} onClose={onClose} onConfirm={onClose} title="정답입니다!" />
+      <MathPopupFactory />
     </>
   );
 };
