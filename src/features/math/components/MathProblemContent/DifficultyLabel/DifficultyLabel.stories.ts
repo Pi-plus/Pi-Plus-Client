caed29cb5bec94ed364370613f06ff35972aa74a
suminhan123 <suminh324@gmail.com>
2024-07-07 16:41:23 +0900

    feat: DifficultLabel 스토리북 추가

---
+++ src/features/math/components/MathProblemContent/DifficultyLabel/DifficultyLabel.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import DifficultyLabel from '@/features/math/components/MathProblemContent/DifficultyLabel/index';
+
+const meta: Meta<typeof DifficultyLabel> = {
+  title: 'Typography/DifficultyLabel',
+  component: DifficultyLabel,
+  argTypes: {
+    difficulty: {
+      control: { type: 'radio', options: ['LOW', 'MEDIUM', 'HIGH'] },
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    difficulty: 'LOW',
+  },
+};
