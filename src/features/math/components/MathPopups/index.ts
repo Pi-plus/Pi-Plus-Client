153ee415ae5707653e17c83d1d9a92c00adff855
suminhan123 <suminh324@gmail.com>
2024-07-07 18:44:58 +0900

    refactor: MathPopup 폴더 생성해 코드 이동

--- src/features/math/components/MathPopups/index.ts
+++ src/features/math/components/MathPopups/index.ts
@@ -1,4 +1,1 @@
-export { default as MathCorrectPopup } from './MathCorrectPopup';
 export { default as MathPopupFactory } from './MathPopupFactory';
-export { default as MathSolutionPopup } from './MathSolutionPopup';
-export { default as MathWrongPopup } from './MathWrongPopup';
--- src/features/math/constants/popups.ts
+++ src/features/math/constants/popups.ts
@@ -1,6 +1,8 @@
 import type { ComponentProps, FunctionComponent } from 'react';
 
-import { MathCorrectPopup, MathSolutionPopup, MathWrongPopup } from '@/features/math/components/MathPopups';
+import MathCorrectPopup from '@/features/math/components/MathPopups/MathCorrectPopup';
+import MathSolutionPopup from '@/features/math/components/MathPopups/MathSolutionPopup';
+import MathWrongPopup from '@/features/math/components/MathPopups/MathWrongPopup';
 
 export const MATH_POPUPS = {
   correct: MathCorrectPopup as FunctionComponent<ComponentProps<typeof MathCorrectPopup>>,
