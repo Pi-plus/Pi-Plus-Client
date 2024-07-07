01c14a0e9dd9f5ea90d411cac7a2eea180e97365
suminhan123 <suminh324@gmail.com>
2024-07-07 19:11:33 +0900

    feat: MathCorrectPopup 스토리북 추가

---
+++ src/features/math/components/MathPopups/MathCorrectPopup/MathCorrectPopup.stories.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Button from '@/components/Button';
+import { MathPopupFactory } from '@/features/math/components/MathPopups';
+import MathCorrectPopup from '@/features/math/components/MathPopups/MathCorrectPopup/index';
+import { MATH_POPUPS } from '@/features/math/constants';
+import { useMathPopups } from '@/features/math/hooks';
+
+const meta: Meta<typeof MathCorrectPopup> = {
+  title: 'Popup/MathCorrectPopup',
+  component: MathCorrectPopup,
+};
+export default meta;
+
+const Template = () => {
+  const popup = useMathPopups();
+  const handleOpenCorrectPopup = () => {
+    popup.openPopup(MATH_POPUPS.correct, {
+      title: '정답입니다!',
+      onClose: () => {
+        popup.closePopup(MATH_POPUPS.correct);
+      },
+      onConfirm: () => {
+        popup.closePopup(MATH_POPUPS.correct);
+      },
+    });
+  };
+  return (
+    <>
+      <div className="w-[30%]">
+        <Button onClick={handleOpenCorrectPopup}>정답 팝업 열기</Button>
+      </div>
+      <MathPopupFactory />
+    </>
+  );
+};
+export const Primary: StoryObj<typeof Template> = {
+  render: () => <Template />,
+};
