93227f056cc2413c7eb5be6b1658e1b7b35fb8e8
suminhan123 <suminh324@gmail.com>
2024-07-14 19:07:57 +0900

    remove: 안쓰는 로그인, 회원가입 탭 상수 삭제

--- src/components/Header/constant.ts
+++ src/components/Header/constant.ts
@@ -2,27 +2,6 @@ import type { TTabContent } from '@/components/Header/type';
 import { LOGIN_ROUTES, MYPAGE_STUDENT_ROUTES, MYPAGE_TEACHER_ROUTES, ROUTES, SIGN_UP_ROUTES } from '@/constants';
 
 /**
- * @descript 로그인 탭 상수
- */
-export const LOGIN_TAB: TTabContent[] = [
-  {
-    label: '로그인',
-    href: `${ROUTES.LOGIN}?step=${LOGIN_ROUTES.SELECT}`,
-    baseUrl: ROUTES.LOGIN,
-  },
-];
-
-/**
- * @descript 회원 가입 탭 상수
- */
-export const SIGN_UP_TAB: TTabContent[] = [
-  {
-    label: '회원가입',
-    href: ROUTES.SIGN_UP,
-    baseUrl: ROUTES.SIGN_UP,
-  },
-];
-/**
  * @description 홈 화면 탭 상수
  */
 export const HOME_TAB: TTabContent[] = [
