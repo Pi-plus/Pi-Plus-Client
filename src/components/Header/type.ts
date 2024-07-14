664e4f10d1a5d2d48dcf280039c88a3bed05c1f9
suminhan123 <suminh324@gmail.com>
2024-07-14 20:03:37 +0900

    feat: 중학교, 고등학교 상수에 대한 타입 생성

--- src/components/Header/type.ts
+++ src/components/Header/type.ts
@@ -6,3 +6,12 @@ export type TTabContent = {
 };
 
 export type TFocusTab = '' | '문제 풀기' | '개념학습';
+
+export type THoverValueOfStudent = 'middle' | 'high' | '';
+export type TValueOfStudent = 'middle' | 'high';
+
+export type TStudentContent = {
+  label: string;
+  href?: string;
+  query?: string;
+};
