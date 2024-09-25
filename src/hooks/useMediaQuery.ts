87b47b518ca407121fd6ef4308d7f2165b9ccfbb
suminhan123 <suminh324@gmail.com>
2024-09-25 17:25:32 +0900

    feat: useMediaQuery 훅 생성

---
+++ src/hooks/useMediaQuery.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+
+export const useMediaQuery = (query: string) => {
+  const getMatches = (query: string): boolean => {
+    // SSR 이슈 방지
+    if (typeof window !== 'undefined') {
+      return window.matchMedia(query).matches;
+    }
+    return false;
+  };
+  const [matches, setMatches] = useState<boolean>(getMatches(query));
+
+  function handleChange() {
+    setMatches(getMatches(query));
+  }
+
+  useEffect(() => {
+    //const media = window.matchMedia(query);
+    handleChange();
+
+    window.addEventListener('resize', handleChange);
+
+    return () => window.removeEventListener('resize', handleChange);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query]);
+
+  return matches;
+};
