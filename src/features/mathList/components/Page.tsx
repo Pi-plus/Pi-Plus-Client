78410dedb71213ee4b6b2b7c4bb967f40887b891
suminhan123 <suminh324@gmail.com>
2024-09-25 18:02:06 +0900

    style: 수학 문제 리스트 페이지에 padding 반응형 적용

--- src/features/mathList/components/Page.tsx
+++ src/features/mathList/components/Page.tsx
@@ -11,7 +11,7 @@ const MathListPage = () => {
   return (
     <>
       {query.math === 'middle1-1' ? (
-        <div className="w-full flex flex-col justify-center mt-9 px-44">
+        <div className="w-full flex flex-col justify-center mt-9 px-60 md:max-lg:px-36 sm:max-md:px-10 max-sm:px-5 min-w-[480px]">
           <MathTable />
         </div>
       ) : (
